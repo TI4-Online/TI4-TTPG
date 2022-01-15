4133f719cee1f08f8d1772b58776faf654757126
Darrell <darrell.anderson@gmail.com>
2022-01-15 12:14:41 -0800

    hex/cartesion translation

---
+++ src/mock/MockVector.js
@@ -0,0 +1,13 @@
+/**
+ * Mock https://api.tabletop-playground.com/classes/_api_.vector.html
+ */
+
+class Vector {
+    constructor(x, y, z) {
+        this.x = x
+        this.y = y
+        this.z = z
+    }
+}
+
+module.exports.Vector = Vector
