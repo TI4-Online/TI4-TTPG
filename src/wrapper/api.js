cc66a44c3e3e5e84388e8b2d609199c09169b309
Darrell <darrell.anderson@gmail.com>
2022-01-21 21:40:23 -0800

    more mocked objects

--- src/wrapper/api.js
+++ src/wrapper/api.js
@@ -3,7 +3,8 @@
 try {
     module.exports = require('@tabletop-playground/api')
 } catch {
-    Object.assign(module.exports, require('../mock/mock-vector'))
-    Object.assign(module.exports, require('../mock/mock-global-scripting-events'))
     Object.assign(module.exports, require('../mock/mock-game-world'))
+    Object.assign(module.exports, require('../mock/mock-global-scripting-events'))
+    Object.assign(module.exports, require('../mock/mock-player'))
+    Object.assign(module.exports, require('../mock/mock-vector'))
 }
