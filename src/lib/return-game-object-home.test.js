9d8e381058bab82887c2d96e39f0c8cbdc140cac
Luke Lewis <llewis@usertesting.com>
2022-01-25 17:32:05 +0000

    Stubs out return methods for returning specific game object types

--- src/lib/return-game-object-home.test.js
+++ src/lib/return-game-object-home.test.js
@@ -2,6 +2,12 @@ const { MockGameObject } = require('../mock/mock-game-object');
 const ReturnGameObjectHome = require('./return-game-object-home');
 
 describe('ReturnGameObjectHome', () => {
+
+  describe('instantiation', () => {
+    it('throws an error when instantiated', () => {
+      expect(() => { new ReturnGameObjectHome() }).toThrow()
+    })
+  });
   
   describe('#returnAll', () => {
     let objectsToReturn
@@ -15,9 +21,76 @@ describe('ReturnGameObjectHome', () => {
       ReturnGameObjectHome.returnAll(objectsToReturn)
     });
 
+    afterEach(() => {
+      jest.clearAllMocks()
+    });
+
     it('should call #return for each object', () => {
       expect(ReturnGameObjectHome.return).toHaveBeenCalledWith(objectsToReturn[0])
       expect(ReturnGameObjectHome.return).toHaveBeenCalledWith(objectsToReturn[1])
+    });
+  });
+
+  describe('#return', () => {
+    const objectTypes = [
+      {
+        name: 'card',
+        templateMetadata: 'card.action:base/direct_hit.2',
+        delegateMethodName: 'returnCard'
+      },
+      {
+        name: 'command token',
+        templateMetadata: 'token.command:base/arborec',
+        delegateMethodName: 'returnToPlayerReinforcements'
+      },
+      {
+        name: 'control token',
+        templateMetadata: 'token.control:base/arborec',
+        delegateMethodName: 'returnToPlayerReinforcements'
+      },
+      {
+        name: 'strategy card',
+        templateMetadata: 'tile.strategy:base/leadership.omega',
+        delegateMethodName: 'returnStrategyCard'
+      },
+      {
+        name: 'system tile',
+        templateMetadata: 'tile.system:base/18',
+        delegateMethodName: 'returnSystemTile'
+      },
+      {
+        name: 'token',
+        templateMetadata: 'token.vuilraith:pok/tear.nekro',
+        delegateMethodName: 'returnToken'
+      },
+      {
+        name: 'unit',
+        templateMetadata: 'unit:base/dreadnought',
+        delegateMethodName: 'returnToPlayerReinforcements'
+      },
+    ]
+
+    objectTypes.forEach(({ name, templateMetadata, delegateMethodName }) => {
+      describe(`returning a ${name}`, () => {
+        let object
+  
+        beforeEach(() => {
+          jest.spyOn(ReturnGameObjectHome, delegateMethodName)
+          object = new MockGameObject({ templateMetadata })
+          ReturnGameObjectHome.return(object)
+        });
+  
+        it(`calls ${delegateMethodName}`, () => {
+          expect(ReturnGameObjectHome[delegateMethodName]).toHaveBeenCalledWith(object)
+        });
+      });
+    })
+
+    describe('returning an unknown object type', () => {
+      it('throws an error', () => {
+        const unknownObject = new MockGameObject({ templateMetadata: 'unknown.thing:fiz.bit/wiz.tot' })
+        expect(() => ReturnGameObjectHome.return(unknownObject)).toThrow()
+      });
     });
   });
 });
