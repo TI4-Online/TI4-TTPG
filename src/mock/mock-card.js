e1d9d3b950c97f3f6177eb7e155904ace8b15282
Darrell <darrell.anderson@gmail.com>
2022-01-27 11:35:04 -0800

    mock Card.isFaceUp

--- src/mock/mock-card.js
+++ src/mock/mock-card.js
@@ -5,10 +5,15 @@ class Card extends GameObject {
     constructor(data) {
         super(data)
         this._cardDetails = data && data.cardDetails || new MockCardDetails()
+        this._faceUp = data && data.faceUp || true
     }
 
     getCardDetails() {
         return this._cardDetails
+    }
+
+    isFaceUp() {
+        return this._faceUp
     }
 }
 
