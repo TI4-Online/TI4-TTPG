8dbfb980f8d9eac5bbcf1a456dca40fd9378b74d
Darrell <darrell.anderson@gmail.com>
2022-01-27 10:20:36 -0800

    deck cardsheet facing metadata isn't clear how one sets it, so workaround 'back side on top' is considered faceup by the natural TTPG rotation to consider that face down.

--- src/mock/mock-api.js
+++ src/mock/mock-api.js
@@ -1,6 +1,7 @@
 // Export under the mock names so tests can be explicit they are not using TTPG objects.
 Object.assign(module.exports, {
     MockCard : require('./mock-card'),
+    MockCardDetails : require('./mock-card-details'),
     MockGameObject : require('./mock-game-object'),
     MockGameWorld : require('./mock-game-world'),
     MockGlobalScriptingEvents : require('./mock-global-scripting-events'),
@@ -12,6 +13,7 @@ Object.assign(module.exports, {
 // Export under the TTPG api names for unaware consumers.
 Object.assign(module.exports, {
     Card : module.exports.MockCard,
+    CardDetails : module.exports.MockCardDetails,
     GameObject : module.exports.MockGameObject,
     GameWorld : module.exports.MockGameWorld,
     GlobalScriptingEvents : module.exports.MockGlobalScriptingEvents,
