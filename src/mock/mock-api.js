b7a18bd5ac12cd756cdf3642866795ef0b202a4f
Darrell <darrell.anderson@gmail.com>
2022-01-27 10:51:53 -0800

    make sure mock world is shared (assuming script globals are shared in node?).  provide world.getAllObjects add/remove methods for tests.

--- src/mock/mock-api.js
+++ src/mock/mock-api.js
@@ -1,6 +1,7 @@
 // Export under the mock names so tests can be explicit they are not using TTPG objects.
 Object.assign(module.exports, {
     MockCard : require('./mock-card'),
+    MockCardDetails : require('./mock-card-details'),
     MockGameObject : require('./mock-game-object'),
     MockGameWorld : require('./mock-game-world'),
     MockGlobalScriptingEvents : require('./mock-global-scripting-events'),
@@ -12,6 +13,7 @@ Object.assign(module.exports, {
 // Export under the TTPG api names for unaware consumers.
 Object.assign(module.exports, {
     Card : module.exports.MockCard,
+    CardDetails : module.exports.MockCardDetails,
     GameObject : module.exports.MockGameObject,
     GameWorld : module.exports.MockGameWorld,
     GlobalScriptingEvents : module.exports.MockGlobalScriptingEvents,
@@ -20,8 +22,12 @@ Object.assign(module.exports, {
     Vector : module.exports.MockVector,
 })
 
+// SHARE global objects.
+const globalEvents = new module.exports.GlobalScriptingEvents()
+const world = new module.exports.GameWorld()
+
 // Create TTPG runtime objects.
 Object.assign(module.exports, {
-    world : new module.exports.GameWorld(),
-    globalEvents : new module.exports.GlobalScriptingEvents(),
+    globalEvents,
+    world,
 })
