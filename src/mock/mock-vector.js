0442eabdd45d951ae2e8a333120a75edf6fa2b5c
Darrell <darrell.anderson@gmail.com>
2022-01-27 17:24:27 -0800

    mock some vector methods

--- src/mock/mock-vector.js
+++ src/mock/mock-vector.js
@@ -7,6 +7,18 @@ class Vector {
         this.y = y
         this.z = z
     }
+
+    subtract(v) {
+        if (Array.isArray(v)) {
+            return new Vector(this.x - v[0], this.y - v[1], this.z - v[2])
+        } else {
+            return new Vector(this.x - v.x, this.y - v.y, this.z - v.z)
+        }
+    }
+
+    magnitudeSquared() {
+        return this.x * this.x + this.y * this.y + this.z * this.z
+    }
 }
 
 module.exports = Vector
