32907347d2410cd620dc5f270dd9b5c9ef93fdae
Darrell <darrell.anderson@gmail.com>
2022-02-04 15:10:15 -0800

    cleanup

--- src/setup/setup-units.js
+++ src/setup/setup-units.js
@@ -52,20 +52,13 @@ const UNIT_DATA = [
     },
 ];
 
+// Desk center [-119.224, 6.05442]
 const SHELF_CENTER_LOCAL_OFFSET = { x: 5.783, y: -55.639, z: 8 };
 const ARC_ORIGIN_LOCAL_OFFSET = { x: -8.845, y: -15.017, z: 0 };
 const DISTANCE_BETWEEN_UNITS = 5.5;
 
 class SetupUnits {
     static setupDesk(playerDesk) {
-        // Desk center [-119.224, 6.05442]
-        // Arc origin [-128.069, -8.963]
-        // const tCenter = new Vector(-119.224, 6.05442, 0);
-        // const tShelfCenter = new Vector(-113.441, -49.585, 0);
-        // const tArcOrigin = new Vector(-128.069, -8.963, 0);
-        // const d = tShelfCenter.subtract(tCenter);
-        // console.log(`${d.x} ${d.y}`);
-
         let o = SHELF_CENTER_LOCAL_OFFSET;
         const shelfCenter = new Vector(o.x, o.y, o.z)
             .rotateAngleAxis(playerDesk.rot.yaw, [0, 0, 1])
@@ -89,22 +82,22 @@ class SetupUnits {
         }
     }
 
-    static _setupUnit(deskData, unitData, pointPosRot) {
+    static _setupUnit(playerDesk, unitData, pointPosRot) {
         const unitNsid = unitData.unitNsid;
         const bagNsid = "bag." + unitNsid;
 
-        const slotColor = PlayerArea.getPlayerSlotColor(deskData.playerSlot);
+        const slotColor = PlayerArea.getPlayerSlotColor(playerDesk.playerSlot);
 
         const bag = Spawn.spawn(bagNsid, pointPosRot.pos, pointPosRot.rot);
         bag.clear(); // paranoia
         bag.setObjectType(ObjectType.Ground);
-        bag.setOwningPlayerSlot(deskData.playerSlot);
+        bag.setOwningPlayerSlot(playerDesk.playerSlot);
         bag.setPrimaryColor(slotColor); // setting owning slot applies default, set again paranoia
 
         for (let i = 0; i < unitData.unitCount; i++) {
             const aboveBag = pointPosRot.pos.add([0, 0, 10 + i]);
             const unit = Spawn.spawn(unitNsid, aboveBag, pointPosRot.rot);
-            unit.setOwningPlayerSlot(deskData.playerSlot);
+            unit.setOwningPlayerSlot(playerDesk.playerSlot);
             unit.setPrimaryColor(slotColor);
             bag.addObjects([unit]);
         }
