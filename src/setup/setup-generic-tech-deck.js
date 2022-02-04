a099d39f6401a3e9f11c51a492920b4423f6d948
Darrell <darrell.anderson@gmail.com>
2022-02-04 10:10:32 -0800

    move to 'top-left' of desk

--- src/setup/setup-generic-tech-deck.js
+++ src/setup/setup-generic-tech-deck.js
@@ -2,10 +2,21 @@ const { Gather } = require("./spawn/gather");
 const { ObjectNamespace } = require("../lib/object-namespace");
 const { Spawn } = require("./spawn/spawn");
 const { ReplaceObjects } = require("./spawn/replace-objects");
-const { Rotator, Vector } = require("../wrapper/api");
+const { Vector } = require("../wrapper/api");
+
+const TECH_DECK_LOCAL_OFFSET = { x: 11, y: -24, z: 7 };
 
 class SetupGenericTechDeck {
     static setupDesk(playerDesk) {
+        const deckPos = new Vector(
+            TECH_DECK_LOCAL_OFFSET.x,
+            TECH_DECK_LOCAL_OFFSET.y,
+            TECH_DECK_LOCAL_OFFSET.z
+        )
+            .rotateAngleAxis(playerDesk.rot.yaw, [0, 0, 1])
+            .add(playerDesk.pos);
+        const deckRot = playerDesk.rot;
+
         const deckNsids = Spawn.getAllNSIDs().filter((nsid) => {
             // Get the DECK nsids, will need to merge into one deck.
             const parsedNsid = ObjectNamespace.parseNsid(nsid);
@@ -20,11 +31,9 @@ class SetupGenericTechDeck {
         deckNsids.sort();
 
         // Spawn the decks, combine into one.
-        const pos = playerDesk.pos;
-        const rot = playerDesk.rot;
         let techDeck = false;
         deckNsids.forEach((deckNsid) => {
-            const deckObj = Spawn.spawn(deckNsid, pos, rot);
+            const deckObj = Spawn.spawn(deckNsid, deckPos, deckRot);
             if (techDeck) {
                 techDeck.addCards(deckObj);
             } else {
