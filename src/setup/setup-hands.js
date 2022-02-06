2f311f63fa65489cc5ee031ea882a034098150ab
Darrell <darrell.anderson@gmail.com>
2022-02-05 20:39:11 -0800

    setup hands.  changing seats via the 'take seat' button now moves camera to seat position.

---
+++ src/setup/setup-hands.js
@@ -0,0 +1,33 @@
+const { AbstractSetup } = require("./abstract-setup");
+const { world } = require("../wrapper/api");
+
+const HAND_LOCAL_OFFSET = {
+    x: -29.7,
+    y: -6,
+    z: 5,
+};
+
+class SetupHands extends AbstractSetup {
+    constructor(playerDesk) {
+        super();
+        this.setPlayerDesk(playerDesk);
+    }
+
+    setup() {
+        const pos = this.playerDesk.localPositionToWorld(HAND_LOCAL_OFFSET);
+        const rot = this.playerDesk.rot;
+        const playerSlot = this.playerDesk.playerSlot;
+
+        // For now use the TTPG wide hand.
+        // Swap for custom model when available.
+        const templateId = "A12757C14B19B37232C9FCAC9B04CEA7";
+        const obj = world.createObjectFromTemplate(templateId, pos);
+        obj.setRotation(rot);
+        obj.setOwningPlayerSlot(playerSlot);
+
+        // The built-in card holder does not show backs.  Leave it be.
+        // We *could* edit json or make a template, but wait for a good one.
+    }
+}
+
+module.exports = { SetupHands };
