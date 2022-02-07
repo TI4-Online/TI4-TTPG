9b158d8b2f8c5d265a17c606e8eb2094191d9dc2
Darrell <darrell.anderson@gmail.com>
2022-02-06 19:55:53 -0800

    split out AuxData 'pair' computation to a new class for getting unit counts and modifiers.  Make an asynchronous version in case it's too expensive.

---
+++ src/lib/unit/auxdata-pair.js
@@ -0,0 +1,318 @@
+const assert = require("../../wrapper/assert");
+const locale = require("../locale");
+const { Broadcast } = require("../broadcast");
+const { Hex } = require("../hex");
+const { UnitAttrs } = require("./unit-attrs");
+const { UnitModifier } = require("./unit-modifier");
+const { UnitPlastic } = require("./unit-plastic");
+const { AuxData } = require("./auxdata");
+
+/**
+ * Given a combat between two players, create and fill in the AuxData
+ * records for each.
+ *
+ * Accounts for:
+ * - player unit upgrades ("Carrier II")
+ * - self-modifiers ("Morale Boost")
+ * - from-opponent-modifiers ("Disable")
+ * - faction abilities ("Fragile")
+ *
+ * It will find all units in the primary hex as well as adjacent hexes,
+ * assigning cardboard tokens to the closest in-hex plastic.
+ *
+ * If the opponent (playerSlot2) is not given, detect it based on plastic.
+ *
+ * This is a VERY expensive process, may want to make it asynchronous.
+ */
+class AuxDataPair {
+    constructor(playerSlot1, playerSlot2, hex, planet) {
+        assert(typeof playerSlot1 === "number");
+        assert(typeof playerSlot2 === "number");
+        assert(!hex || typeof hex === "string");
+        assert(!planet || typeof planet === "string");
+
+        this._playerSlot1 = playerSlot1;
+        this._playerSlot2 = playerSlot2;
+        this._hex = hex;
+        this._planet = planet;
+
+        this._adjHexes = new Set();
+
+        this._allPlastic = false;
+        this._hexPlastic = false;
+        this._adjPlastic = false;
+
+        this._aux1 = false;
+        this._aux2 = false;
+    }
+
+    _getProcessQueue() {
+        return [
+            () => {
+                this.computeAdjHexes();
+            },
+            () => {
+                this.computePlatic();
+            },
+            () => {
+                this.computeOpponent();
+                this._aux1 = new AuxData(this._playerSlot1);
+                this._aux2 = new AuxData(this._playerSlot2);
+            },
+            () => {
+                this.computeSelfUnitCounts(this._aux1);
+            },
+            () => {
+                this.computeSelfUnitCounts(this._aux2);
+            },
+            () => {
+                this.computeSelfUnitUpgrades(this._aux1);
+            },
+            () => {
+                this.computeSelfUnitUpgrades(this._aux2);
+            },
+            () => {
+                this.computeSelfUnitAbilities(this._aux1, this._aux2);
+            },
+            () => {
+                this.computeSelfUnitAbilities(this._aux2, this._aux1);
+            },
+            () => {
+                this.computeSelfUnitModifiers(this._aux1, this._aux2);
+            },
+            () => {
+                this.computeSelfUnitModifiers(this._aux2, this._aux1);
+            },
+            () => {
+                this.computeSelfFactionAbilities(this._aux1, this._aux2);
+            },
+            () => {
+                this.computeSelfFactionAbilities(this._aux2, this._aux1);
+            },
+            () => {
+                this.applySelfUnitModifiers(this._aux1, this._aux2);
+            },
+            () => {
+                this.applySelfUnitModifiers(this._aux2, this._aux1);
+            },
+        ];
+    }
+
+    getPairSync() {
+        const processQueue = this._getProcessQueue();
+        for (const processEntry of processQueue) {
+            processEntry();
+        }
+        return [this._aux1, this._aux2];
+    }
+
+    getPairAsync(callback) {
+        const processQueue = this._getProcessQueue();
+        const processNext = () => {
+            const processEntry = processQueue.shift();
+            if (!processEntry) {
+                callback(this._aux1, this._aux2);
+            }
+            setTimeout(processNext, 10);
+        };
+        processNext();
+    }
+
+    /**
+     * If given a hex but not a planet, compute the set of adjacent hexes.
+     */
+    computeAdjHexes() {
+        if (this._hex && !this._planet) {
+            for (const adjHex of Hex.neighbors(this._hex)) {
+                this._adjHexes.add(adjHex);
+            }
+            // TODO XXX WORMHOLE ADJACENCY
+            // TODO XXX HYPERLANE ADJACENCY
+        }
+    }
+
+    /**
+     * Get all plasic in hex and adjacent hexes.  Restrict to "on planet"
+     * plastic if a planet combat.
+     */
+    computePlatic() {
+        this._allPlastic = this._hex ? UnitPlastic.getAll() : [];
+        this._hexPlastic = this._allPlastic.filter(
+            (plastic) => plastic.hex === this._hex
+        );
+        this._adjPlastic = this._allPlastic.filter((plastic) =>
+            this._adjHexes.has(plastic.hex)
+        );
+        UnitPlastic.assignTokens(this._hexPlastic);
+        UnitPlastic.assignTokens(this._adjPlastic);
+        if (this._planet) {
+            UnitPlastic.assignPlanets(this._planet);
+            this._planethexPlastic = this._planethexPlastic.filter(
+                (plastic) => plastic.planet === this._planetplanet
+            );
+        }
+    }
+
+    /**
+     * If no opponent was given compute one based on "not us" plastic in fight.
+     */
+    computeOpponent() {
+        if (this._playerSlot2 >= 0) {
+            return; // already have an oppoent
+        }
+        // Units pruned down to just those involved in space/planet fight.
+        for (const plastic of this._hexPlastic) {
+            if (plastic.owningPlayerSlot === this._playerSlot1) {
+                continue; // ignore our own units
+            }
+            if (plastic.owningPlayerSlot === this._playerSlot2) {
+                continue; // ignore if we already think this is opponent
+            }
+            if (this._playerSlot2 < 0) {
+                this._playerSlot2 = plastic.owningPlayerSlot;
+            } else {
+                // Multiple opponents!
+                Broadcast.broadcastAll(locale("ui.error.too_many_opponents"));
+                this._playerSlot2 = -1;
+                break;
+            }
+        }
+    }
+
+    /**
+     * Count units belonging to this player in the main and ajacent hexes.
+     *
+     * @param {AuxData} selfAuxData
+     */
+    computeSelfUnitCounts(selfAuxData) {
+        assert(selfAuxData instanceof AuxData);
+
+        // Get hex and adjacent plastic for this player.
+        // Also get counts, beware of x3 tokens!
+        const playerHexPlastic = this._hexPlastic.filter(
+            (plastic) => plastic.owningPlayerSlot == selfAuxData.playerSlot
+        );
+        selfAuxData.plastic.push(...playerHexPlastic);
+        for (const plastic of playerHexPlastic) {
+            const count = selfAuxData.count(plastic.unit);
+            selfAuxData.overrideCount(plastic.unit, count + plastic.count);
+        }
+        const playerAdjPlastic = this._adjPlastic.filter(
+            (plastic) => plastic.owningPlayerSlot == selfAuxData.playerSlot
+        );
+        selfAuxData.adjacentPlastic.push(...playerAdjPlastic);
+        for (const plastic of playerAdjPlastic) {
+            const count = selfAuxData.adjacentCount(plastic.unit);
+            selfAuxData.overrideAdjacentCount(
+                plastic.unit,
+                count + plastic.count
+            );
+        }
+    }
+
+    /**
+     * Gather and apply applicable unit upgrades.
+     *
+     * @param {AuxData} selfAuxData
+     */
+    computeSelfUnitUpgrades(selfAuxData) {
+        assert(selfAuxData instanceof AuxData);
+
+        // Apply unit upgrades.
+        const upgrades = UnitAttrs.getPlayerUnitUpgrades(
+            selfAuxData.playerSlot
+        );
+        for (const upgrade of upgrades) {
+            selfAuxData.unitAttrsSet.upgrade(upgrade);
+        }
+    }
+
+    /**
+     * Gather BUT DO NOT APPLY unit-ability unit modifiers.
+     * A unit-ability unit modifier are things like flagship abilities.
+     *
+     * @param {AuxData} selfAuxData
+     */
+    computeSelfUnitAbilities(selfAuxData) {
+        for (const unitAttrs of selfAuxData.unitAttrsSet.values()) {
+            if (!selfAuxData.has(unitAttrs.raw.unit)) {
+                continue; // not present
+            }
+            if (!unitAttrs.raw.unitAbility) {
+                continue; // no ability
+            }
+            const unitModifier = UnitModifier.getUnitAbilityUnitModifier(
+                unitAttrs.raw.unitAbility
+            );
+            if (unitModifier) {
+                selfAuxData.unitModifiers.push(unitModifier);
+            }
+        }
+    }
+
+    /**
+     * Gather BUT DO NOT APPLY card-given unit modifiers.
+     *
+     * @param {AuxData} selfAuxData
+     * @param {AuxData} opponentAuxData
+     */
+    computeSelfUnitModifiers(selfAuxData, opponentAuxData) {
+        assert(selfAuxData instanceof AuxData);
+        assert(opponentAuxData instanceof AuxData);
+
+        // Get modifiers.  For each perspective get "self modifiers" and
+        // "opponent modifiers applied to opponent".
+        const modifiersSelf = UnitModifier.getPlayerUnitModifiers(
+            selfAuxData.playerSlot,
+            "self"
+        );
+        const modifiersOpponent = UnitModifier.getPlayerUnitModifiers(
+            opponentAuxData.playerSlot,
+            "opponent"
+        );
+        selfAuxData.unitModifiers.push(...modifiersSelf);
+        selfAuxData.unitModifiers.push(...modifiersOpponent);
+    }
+
+    /**
+     * Gather BUT DO NOT APPLY faction-ability unit modifiers.
+     *
+     * @param {AuxData} selfAuxData
+     * @param {AuxData} opponentAuxData
+     */
+    computeSelfFactionAbilities(selfAuxData, opponentAuxData) {
+        assert(selfAuxData instanceof AuxData);
+        assert(opponentAuxData instanceof AuxData);
+
+        // TODO XXX LOOK UP FACTION BY PLAYER SLOT, ADD MODIFIERS TO AUX.FACTIONABILITIES!
+        for (const factionAbility of []) {
+            const unitModifier =
+                UnitModifier.getFactionAbilityUnitModifier(factionAbility);
+            if (unitModifier) {
+                selfAuxData.unitModifiers.push(unitModifier);
+            }
+        }
+        // TODO XXX Repeat for opponent's "opponent" modifiers.
+    }
+
+    /**
+     * Delay this until after computing unit counts for self AND opponent!
+     *
+     * @param {AuxData} selfAuxData
+     * @param {AuxData} opponentAuxData
+     */
+    applySelfUnitModifiers(selfAuxData, opponentAuxData) {
+        assert(selfAuxData instanceof AuxData);
+        assert(opponentAuxData instanceof AuxData);
+
+        UnitModifier.sortPriorityOrder(selfAuxData.unitModifiers);
+        for (const unitModifier of selfAuxData.unitModifiers) {
+            unitModifier.apply(selfAuxData.unitAttrsSet, {
+                self: selfAuxData,
+                opponent: opponentAuxData,
+            });
+        }
+    }
+}
+
+module.exports = { AuxDataPair };
---
+++ src/lib/unit/auxdata-pair.test.js
@@ -0,0 +1,124 @@
+const assert = require("../../wrapper/assert");
+const { AuxData } = require("./auxdata");
+const { AuxDataPair } = require("./auxdata-pair");
+const {
+    MockCard,
+    MockCardDetails,
+    MockGameObject,
+    MockVector,
+    world,
+} = require("../../wrapper/api");
+
+it("getPairSync", () => {
+    const selfPlayerSlot = 7;
+    const opponentPlayerSlot = 8;
+
+    // Place a few units and tokens.
+    world.__addObject(
+        new MockGameObject({
+            templateMetadata: "unit:base/fighter",
+            owningPlayerSlot: selfPlayerSlot,
+        })
+    );
+    world.__addObject(
+        new MockGameObject({
+            templateMetadata: "unit:base/fighter",
+            owningPlayerSlot: opponentPlayerSlot,
+            position: new MockVector(0.5, 0, 0), // further from tokens than above
+        })
+    );
+    world.__addObject(
+        new MockGameObject({
+            templateMetadata: "token:base/fighter_3",
+        })
+    );
+    world.__addObject(
+        new MockGameObject({
+            templateMetadata: "token:base/infantry_1",
+        })
+    );
+    world.__addObject(
+        new MockGameObject({
+            templateMetadata: "unit:base/pds",
+            owningPlayerSlot: selfPlayerSlot,
+        })
+    );
+    world.__addObject(
+        new MockGameObject({
+            templateMetadata: "unit:base/pds",
+            owningPlayerSlot: opponentPlayerSlot,
+        })
+    );
+
+    // Carrier 2
+    world.__addObject(
+        new MockCard({
+            cardDetails: new MockCardDetails({
+                metadata: "card.technology.unit_upgrade:base/carrier_2",
+            }),
+            owningPlayerSlot: selfPlayerSlot,
+        })
+    );
+
+    // Morale boost!
+    world.__addObject(
+        new MockCard({
+            cardDetails: new MockCardDetails({
+                metadata: "card.action:base/morale_boost.3",
+            }),
+            owningPlayerSlot: selfPlayerSlot,
+        })
+    );
+
+    // Opponent cancels our PDS planetary shield.
+    world.__addObject(
+        new MockCard({
+            cardDetails: new MockCardDetails({
+                metadata: "card.action:base/disable",
+            }),
+            owningPlayerSlot: opponentPlayerSlot,
+        })
+    );
+
+    let aux1, aux2;
+    try {
+        [aux1, aux2] = new AuxDataPair(
+            selfPlayerSlot,
+            -1,
+            "<0,0,0>",
+            false
+        ).getPairSync();
+    } finally {
+        for (const gameObject of world.getAllObjects()) {
+            world.__removeObject(gameObject);
+        }
+    }
+
+    // Identified opponent?
+    assert.equal(aux2.playerSlot, opponentPlayerSlot);
+
+    // Basic finding.
+    assert.equal(aux1.count("fighter"), 4);
+    assert.equal(
+        aux1.unitAttrsSet.get("carrier").raw.localeName,
+        "unit.carrier_2"
+    );
+
+    const modifierNames = aux1.unitModifiers.map((x) => x.raw.localeName);
+    assert.equal(modifierNames.length, 2);
+    assert(modifierNames.includes("unit_modifier.name.morale_boost"));
+    assert(modifierNames.includes("unit_modifier.name.disable"));
+
+    assert.equal(aux2.count("fighter"), 1);
+
+    // Verify self modifier.
+    assert.equal(aux1.count("fighter"), 4);
+
+    // Verify opponent's opponent modifier.
+    assert(!aux1.unitAttrsSet.get("pds").raw.planetaryShield); // disabled
+    assert(aux2.unitAttrsSet.get("pds").raw.planetaryShield); // opponent still has it
+});
+
+it("constructor", () => {
+    new AuxData(-1);
+});
