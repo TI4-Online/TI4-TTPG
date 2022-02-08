ef647859f114accd3a204a1e0d2163d0ebb750b5
Darrell <darrell.anderson@gmail.com>
2022-02-07 23:13:54 -0800

    place-trade-goods-and-set-turns parts

---
+++ src/lib/phase/find-turn-order.test.js
@@ -0,0 +1,47 @@
+const assert = require("assert");
+const { FindTurnOrder } = require("./find-turn-order");
+const { PlayerDesk } = require("../player-desk");
+const { MockGameObject, world } = require("../../wrapper/api");
+
+it("order", () => {
+    const playerDesks = PlayerDesk.getPlayerDesks();
+    assert(playerDesks.length > 2);
+    try {
+        world.__addObject(
+            new MockGameObject({
+                position: playerDesks[0].pos,
+                templateMetadata: "tile.strategy:base/diplomacy.errata",
+            })
+        );
+        world.__addObject(
+            new MockGameObject({
+                position: playerDesks[0].pos,
+                templateMetadata: "tile.strategy:base/imperial",
+            })
+        );
+        world.__addObject(
+            new MockGameObject({
+                position: playerDesks[1].pos,
+                templateMetadata: "tile.strategy:base/trade",
+            })
+        );
+        world.__addObject(
+            new MockGameObject({
+                position: playerDesks[1].pos,
+                templateMetadata: "tile.strategy:base/technology",
+            })
+        );
+        world.__addObject(
+            new MockGameObject({
+                position: playerDesks[2].pos,
+                templateMetadata: "token.naalu:base/zero",
+            })
+        );
+        const order = FindTurnOrder.order();
+        assert.equal(order[0], playerDesks[2].playerSlot); // zero token
+        assert.equal(order[1], playerDesks[0].playerSlot); // diplomacy
+        assert.equal(order[2], playerDesks[1].playerSlot); // trade
+    } finally {
+        world.__clear();
+    }
+});
---
+++ src/lib/phase/place-tradegood-unpicked.js
@@ -0,0 +1,49 @@
+const assert = require("../../wrapper/assert-wrapper");
+const { FindTurnOrder } = require("./find-turn-order");
+const { Spawn } = require("../../setup/spawn/spawn");
+const { ObjectNamespace } = require("../object-namespace");
+const { GameObject, Rotator, Vector, world } = require("../../wrapper/api");
+
+const TRADEGOOD_NSID = "token:base/tradegood_1";
+const TRADEGOOD_ROLL = 180;
+
+/**
+ * Place a tradegood on an unpicked strategy card.
+ */
+class PlaceTradegoodUnpicked {
+    constructor() {
+        throw new Error("static only");
+    }
+
+    static placeOne(strategyCard) {
+        assert(strategyCard instanceof GameObject);
+        assert(ObjectNamespace.isStrategyCard(strategyCard));
+
+        const noiseD = 2;
+        const noise = new Vector(
+            Math.random() * noiseD * 2 - noiseD,
+            Math.random() * noiseD * 2 - noiseD,
+            5
+        );
+        const pos = strategyCard.getPosition().add(noise);
+        const rot = new Rotator(0, 0, TRADEGOOD_ROLL);
+        Spawn.spawn(TRADEGOOD_NSID, pos, rot);
+    }
+
+    static placeAll() {
+        for (const obj of world.getAllObjects()) {
+            if (obj.getContainer()) {
+                continue; // ignore inside containers
+            }
+            if (!ObjectNamespace.isStrategyCard(obj)) {
+                continue; // not a strategy card.
+            }
+            if (FindTurnOrder.isStrategyCardPicked(obj)) {
+                continue; // picked, ignore it
+            }
+            this.placeOne(obj);
+        }
+    }
+}
+
+module.exports = { PlaceTradegoodUnpicked };
