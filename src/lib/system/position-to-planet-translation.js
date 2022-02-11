e21f9d1e541567434805e56ee12c732dd87e70d5
11qats <11qats@gmail.com>
2022-02-11 13:52:08 -0500

    use worldPositionToLocal instead of reinventing the wheel

--- src/lib/system/position-to-planet-translation.js
+++ src/lib/system/position-to-planet-translation.js
@@ -3,17 +3,16 @@ const { ObjectNamespace } = require("../object-namespace");
 const { Vector, world, Color } = require("@tabletop-playground/api");
 const SYSTEM_ATTRS = require("./system.data");
 const { Hex } = require("../hex");
-
-const SCALE = Hex.SCALE;
+const { Facing } = require("../facing");
 
 // TODO: mirage
 // TODO: better Clan of Saar home system
 
 // constants used to draw debug spheres
-const DEBUG_COLOR = new Color(0, 0, 0);
+const DEBUG_COLOR = new Color(255, 0, 255);
 const DEBUG_DURATION = 10;
 const DEBUG_THICKNESS = 0.1;
-const TILE_HEIGHT = 0.253 * SCALE;
+const TILE_HEIGHT = 0.253;
 
 // increase this to make determing if a position is on a planet more lenient
 // decrease it to make the check more strict
@@ -33,189 +32,70 @@ const STANDARD_TWO_PLANET_SYSTEMS = [
 ];
 const STANDARD_THREE_PLANET_SYSTEMS = [75, 76];
 
-/**
- * Convert cartesian coordinates to polar coordinates.
- *
- * If x is 0, theta is automatically set to 0.
- *
- * @param {Number} x
- * @param {Number} y
- * @returns {{r: Number, theta: Number}}
- */
-function getPolarCoords(x, y) {
-    return {
-        r: Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)),
-        theta: x === 0 ? 0 : Math.atan(y / x),
-    };
-}
-
-/**
- * Convert polar coordinates to cartesian coordinates.
- *
- * There is probably a better way to handle this, but I needed to multiply the
- * x and y outputs if the planet was above the center of the system tile.
- * @param {Number} r
- * @param {Number} theta
- * @param {boolean} above
- * @returns {{x: Number, y: Number}}
- */
-function getCartesianCoords(r, theta, above) {
-    const mult = above ? -1 : 1;
-    return {
-        x: mult * r * Math.cos(theta),
-        y: mult * r * Math.sin(theta),
-    };
-}
-
-/**
- * Given a System return the position (in polar coordinates) of each of its
- * planets relative to the center of the tile and the radius of each planet.
- *
- * Accounts for the scale of the system tiles.
- *
- * @param {System} system
- * @returns {[{polarCoords: {r: Number, theta: Numner}, r: Number}]}
- */
-function getPlanetLocationsHelper(system) {
+function getPlanetHelper(system) {
     const tile = system.tile;
-
     if (tile === 11) {
         // Clan of Saar Home System
         return [
-            {
-                polarCoords: getPolarCoords(SCALE * 2, SCALE * -1.25),
-                r: SCALE * 1.75,
-            },
-            {
-                polarCoords: getPolarCoords(SCALE * -1.8, SCALE * 1.75),
-                r: SCALE * 1.75,
-            },
+            { x: 2, y: -1.25, r: 1.75 },
+            { x: -1.8, y: 1.75, r: 1.75 },
         ];
     } else if (tile === 16) {
         // Hacan Home System
         return [
-            {
-                polarCoords: getPolarCoords(SCALE * 0.5, SCALE * -2.75),
-                r: SCALE * 2,
-            },
-            {
-                polarCoords: getPolarCoords(SCALE * 2.3, SCALE * 1.3),
-                r: SCALE * 2,
-            },
-            {
-                polarCoords: getPolarCoords(SCALE * -2.4, SCALE * 1.9),
-                r: SCALE * 2,
-            },
+            { x: 0.5, y: -2.75, r: 2 },
+            { x: 2.3, y: 1.3, r: 2 },
+            { x: -2.4, y: 1.9, r: 2 },
         ];
     } else if (tile === 18) {
         // Mecatol Rex
-        return [{ polarCoords: getPolarCoords(0, 0), r: SCALE * 4 }];
+        return [{ x: 0, y: 0, r: 4 }];
     } else if (tile === 51) {
         // Creuss Home System
-        return [{ polarCoords: getPolarCoords(SCALE, 0), r: SCALE * 2 }];
+        return [{ x: 1, y: 0, r: 2 }];
     } else if (tile === 55) {
         // Elysium Titans Home System
-        return [
-            {
-                polarCoords: getPolarCoords(SCALE * 0.75, 0),
-                r: SCALE * 3.25,
-            },
-        ];
+        return [{ x: 0.75, y: 0, r: 3.25 }];
     } else if (tile === 58) {
         // Argent Home System
         return [
-            {
-                polarCoords: getPolarCoords(SCALE * 0.5, SCALE * -2.75),
-                r: SCALE * 2,
-            },
-            {
-                polarCoords: getPolarCoords(SCALE * 2.3, SCALE * 1.3),
-                r: SCALE * 2,
-            },
-            {
-                polarCoords: getPolarCoords(SCALE * -2.5, SCALE * 1.7),
-                r: SCALE * 2,
-            },
+            { x: 0.5, y: -2.75, r: 2 },
+            { x: 2.3, y: 1.3, r: 2 },
+            { x: -2.5, y: 1.7, r: 2 },
         ];
     } else if (tile === 65 || tile === 66) {
         // Hope's End or Primor
-        return [{ polarCoords: getPolarCoords(0, 0), r: SCALE * 3.25 }];
+        return [{ x: 0, y: 0, r: 3.25 }];
     } else if (tile === 67) {
         // Cormund
-        return [
-            {
-                polarCoords: getPolarCoords(SCALE * 0.7, -SCALE),
-                r: SCALE * 2,
-            },
-        ];
+        return [{ x: 0.7, y: -1, r: 2 }];
     } else if (tile === 68) {
         // Everra
-        return [
-            {
-                polarCoords: getPolarCoords(SCALE * 0.5, -SCALE),
-                r: SCALE * 2,
-            },
-        ];
+        return [{ x: 0.5, y: -1, r: 2 }];
     } else if (tile == 82) {
         // Malice
-        return [
-            {
-                polarCoords: getPolarCoords(SCALE * 1.2, SCALE),
-                r: SCALE * 2,
-            },
-        ];
+        return [{ x: 1.2, y: 1, r: 2 }];
     } else if (STANDARD_ONE_PLANET_HOME_SYSTEMS.indexOf(tile) > -1) {
-        return [
-            {
-                polarCoords: getPolarCoords(SCALE * 0.65, 0),
-                r: SCALE * 2,
-            },
-        ];
+        return [{ x: 0.65, y: 0, r: 2 }];
     } else if (STANDARD_ONE_PLANET_SYSTEMS.indexOf(tile) > -1) {
-        return [{ polarCoords: getPolarCoords(0, 0), r: SCALE * 2 }];
+        return [{ x: 0, y: 0, r: 2 }];
     } else if (ONE_PLANET_WITH_WORMHOLE_SYSTEMS.indexOf(tile) > -1) {
-        return [
-            {
-                polarCoords: getPolarCoords(SCALE * 2, SCALE * -1.25),
-                r: SCALE * 2,
-            },
-        ];
+        return [{ x: 2, y: -1.25, r: 2 }];
     } else if (STANDARD_TWO_PLANET_HOME_SYSTEMS.indexOf(tile) > -1) {
         return [
-            {
-                polarCoords: getPolarCoords(SCALE * 2, SCALE * -1.25),
-                r: SCALE * 2,
-            },
-            {
-                polarCoords: getPolarCoords(SCALE * -1.8, SCALE * 1.9),
-                r: SCALE * 2,
-            },
+            { x: 2, y: -1.25, r: 2 },
+            { x: -1.8, y: 1.9, r: 2 },
         ];
     } else if (STANDARD_TWO_PLANET_SYSTEMS.indexOf(tile) > -1) {
         return [
-            {
-                polarCoords: getPolarCoords(SCALE * 2, SCALE * -1.25),
-                r: SCALE * 2,
-            },
-            {
-                polarCoords: getPolarCoords(SCALE * -2, SCALE),
-                r: SCALE * 2,
-            },
+            { x: 2, y: -1.25, r: 2 },
+            { x: -2, y: 1, r: 2 },
         ];
     } else if (STANDARD_THREE_PLANET_SYSTEMS.indexOf(tile) > -1) {
         return [
-            {
-                polarCoords: getPolarCoords(SCALE * 0.5, SCALE * -3),
-                r: SCALE * 2,
-            },
-            {
-                polarCoords: getPolarCoords(SCALE * 2, SCALE * 1.5),
-                r: SCALE * 2,
-            },
-            {
-                polarCoords: getPolarCoords(SCALE * -2.7, SCALE * 1.65),
-                r: SCALE * 2,
-            },
+            { x: 0.5, y: -3, r: 2 },
+            { x: 2, y: 1.5, r: 2 },
+            { x: -2.7, y: 1.65, r: 2 },
         ];
     } else {
         return [];
@@ -223,47 +103,36 @@ function getPlanetLocationsHelper(system) {
 }
 
 /**
- * Given a System, its x, y position, rotation and an array of destroyed planets
- * in the system return an array of the x, y positions and radii of all planets
- * thatin the System.
+ *  Draws a debug sphere centered at (x, y, table height) with radius r.
  *
- * @param {System} system
- * @param {Number} X
- * @param {Number} Y
- * @param {Number} rotation
- * @param {[Number]} destroyedPlanetIndices
- * @returns {[{x: Number, y: Number, r: Number}]}
+ * @param {{x: Number, y: Number, r: Number}} planetPos
  */
-function getPlanetLocations(system, X, Y, rotation, destroyedPlanetIndices) {
-    const unrotatedPlanetLocations = getPlanetLocationsHelper(system);
-    const rotationRadians = Math.round(rotation) * (Math.PI / 180);
-    const planetLocations = unrotatedPlanetLocations.map(
-        (element, index, arr) => {
-            const theta = element.polarCoords.theta;
-            const r = element.polarCoords.r;
-            const rotatedPos = getCartesianCoords(
-                r,
-                theta + rotationRadians,
-                index === arr.length - 1 && arr.length > 1 // is the planet above the center of the tile
-            );
-            return { x: rotatedPos.x + X, y: rotatedPos.y + Y, r: element.r };
-        }
+function drawSphereAroundPlanet(planetPos, planetRadius) {
+    const pos = new Vector(
+        planetPos.x,
+        planetPos.y,
+        world.getTableHeight() + TILE_HEIGHT
     );
-    return planetLocations.filter(
-        (_element, index) => destroyedPlanetIndices.indexOf(index) === -1
+    world.drawDebugSphere(
+        pos,
+        planetRadius,
+        DEBUG_COLOR,
+        DEBUG_DURATION,
+        DEBUG_THICKNESS
     );
 }
 
 /**
- * Given a system tile number, return an array of the planet names in the
+ * Given a System, return an array of the planet names in the
  * original schema. This is necessary to check if planets have been destroyed
  *
- * @param {Number} tile
+ * @param {System} system
  * @returns {[string]}
  */
-function getOriginalSystemPlanets(tile) {
+function getOriginalSystemPlanets(system) {
+    const tile = system.tile;
     for (const rawAttrs of SYSTEM_ATTRS) {
-        if (rawAttrs.tile === tile) {
+        if (rawAttrs.tile === tile && rawAttrs.planets) {
             return rawAttrs.planets.map((element) => {
                 return element.localeName;
             });
@@ -272,16 +141,11 @@ function getOriginalSystemPlanets(tile) {
 }
 
 /**
- * Returns the System, if any, that pos is over top of along with the x, y
- * coordinates and rotation of the system object and the names of all planets
- * in the system schema.
+ * Returns the System, if any, that pos is over top of along with the
+ * local position of pos relative to the System.
  *
  * @param {Vector} pos
- * @returns {{system: System,
- *            originalPlanets: [string],
- *            x: Number,
- *            y: number,
- *            rotation: number}}
+ * @returns {{system: System, localPos: Vector}}
  */
 function getSystem(pos) {
     const dst = new Vector(pos.x, pos.y, world.getTableHeight() - 5);
@@ -292,10 +156,7 @@ function getSystem(pos) {
             const pos = hit.object.getPosition();
             return {
                 system: System.getByTileNumber(parsedSystem.tile),
-                originalPlanets: getOriginalSystemPlanets(parsedSystem.tile),
-                x: pos.x,
-                y: pos.y,
-                rotation: hit.object.getRotation().yaw,
+                obj: hit.object,
             };
         }
     }
@@ -317,7 +178,7 @@ function distance(src, dest) {
 
 /**
  * Returns true if point lies inside a circle.
- * With a tolerance of RADIUS_BUFFER * Hex.SCALE
+ * With a tolerance of RADIUS_BUFFER
  *
  * Not using TTPG built in Vector distance because we don't care about the
  * Z dimension.
@@ -327,144 +188,112 @@ function distance(src, dest) {
  * @returns {Number}
  */
 function withinCircle(circle, point) {
-    return distance(circle, point) <= circle.r + RADIUS_BUFFER * SCALE;
+    return distance(circle, point) <= circle.r + RADIUS_BUFFER;
 }
 
 /**
- * Given the current System object and the initial array of planet names in the
- * system which if any planets were destroyed. Returns the index in the original
- * System planets array of the destroyed system, or an empty array if there
- * were no destroyed planets.
+ * Given a System returns the index in the original System planets array of
+ * each destroyed planet in the system, or an empty array if there were no
+ * destroyed planets.
  *
- * @param {System} currSystem
- * @param {[string]} originalPlanets
+ * @param {System} system
  * @returns {[Number]}
  */
-function getDestroyedPlanets(currSystem, originalPlanets) {
-    const currPlanetNames = currSystem.planets.map(
+function getDestroyedPlanets(system) {
+    const originalPlanets = getOriginalSystemPlanets(system);
+    const currPlanetNames = system.planets.map(
         (element) => element.raw.localeName
     );
 
     let destroyedPlanets = [];
-    originalPlanets.forEach((element, index) => {
-        if (currPlanetNames.indexOf(element) === -1) {
-            destroyedPlanets.push(index);
-        }
-    });
+    if (originalPlanets) {
+        originalPlanets.forEach((element, index) => {
+            if (currPlanetNames.indexOf(element) === -1) {
+                destroyedPlanets.push(index);
+            }
+        });
+    }
     return destroyedPlanets;
 }
 
-/**
- * Get the Planet that is closest to pos, if pos is a point on a system tile.
- *
- * If debug is true and there is more than one planet in the system, a debug
- * sphere) will be drawn around the closest planet.
- *
- * @param {Vector} pos
- * @param {boolean} debug
- * @returns {Planet}
- */
-function getClosestPlanet(pos, debug) {
-    const system = getSystem(pos);
-    if (system) {
-        const currSystemPlanets = system.system.planets;
-
-        if (currSystemPlanets.length === 1) {
-            // only one planet therefore must be the closest planet
-            return currSystemPlanets[0];
-        } else if (currSystemPlanets.length > 1) {
-            const destroyedPlanets = getDestroyedPlanets(
-                system.system,
-                system.originalPlanets
-            );
-
-            let planetLocations = getPlanetLocations(
-                system.system,
-                system.x,
-                system.y,
-                system.rotation,
-                destroyedPlanets
-            );
-
-            const distances = planetLocations.map((element) =>
-                distance(element, pos)
-            );
-            const shortestDistanceIndex = distances.indexOf(
-                Math.min(...distances)
-            );
-
-            if (debug) {
-                // draw a sphere around the closest planet
-                const planetLocation = planetLocations[shortestDistanceIndex];
-                const pos = new Vector(
-                    planetLocation.x,
-                    planetLocation.y,
-                    world.getTableHeight() + TILE_HEIGHT
-                );
-                world.drawDebugSphere(
-                    pos,
-                    planetLocation.r + RADIUS_BUFFER * SCALE,
-                    DEBUG_COLOR,
-                    DEBUG_DURATION,
-                    DEBUG_THICKNESS
-                );
-            }
-            return currSystemPlanets[shortestDistanceIndex];
-        }
+function getLocalPosition(obj, pos) {
+    const localPosition = obj.worldPositionToLocal(pos);
+    if (Facing.isFaceUp(obj)) {
+        return localPosition;
+    } else {
+        const correctedPosition = new Vector(
+            localPosition.x,
+            localPosition.y * -1,
+            localPosition.z
+        );
+        return correctedPosition;
     }
 }
 
-/**
- * Get the Planet, if any, that pos is exactly overtop of.
- *
- * If debug is true, a sphere (visible only to the host) will be drawn around
- * the planet that pos is overtop of.
- *
- * @param {Vector} pos
- * @param {boolean} debug
- * @returns {Planet}
- */
+function getWorldPosition(obj, pos) {
+    if (Facing.isFaceUp(obj)) {
+        return obj.localPositionToWorld(pos);
+    } else {
+        const correctedPosition = new Vector(pos.x, pos.y * -1, pos.z);
+        return obj.localPositionToWorld(correctedPosition);
+    }
+}
+
+function getClosestPlanet(pos, debug) {
+    const system = getSystem(pos);
+    if (system && system.system.planets.length > 0) {
+        const localPos = getLocalPosition(system.obj, pos);
+        const planetPositions = getPlanetHelper(system.system);
+        const destroyedPlanets = getDestroyedPlanets(system.system);
+
+        const remainingPlanetPositions = planetPositions.filter(
+            (_element, index) => destroyedPlanets.indexOf(index) === -1
+        );
+
+        const distances = remainingPlanetPositions.map((element) =>
+            distance(element, localPos)
+        );
+
+        const closestPlanetIndex = distances.indexOf(Math.min(...distances));
+
+        if (debug) {
+            const closestPos = remainingPlanetPositions[closestPlanetIndex];
+            const worldPos = getWorldPosition(system.obj, closestPos);
+
+            // convert the planet radius to world
+            const planetRadius = (RADIUS_BUFFER + closestPos.r) * Hex.SCALE;
+            drawSphereAroundPlanet(worldPos, planetRadius);
+        }
+
+        return system.system.planets[closestPlanetIndex];
+    }
+}
+
 function getExactPlanet(pos, debug) {
     const system = getSystem(pos);
-    if (system) {
-        const currSystemPlanets = system.system.planets;
-        if (currSystemPlanets.length >= 1) {
-            const destroyedPlanets = getDestroyedPlanets(
-                system.system,
-                system.originalPlanets
-            );
+    if (system && system.system.planets.length > 0) {
+        const localPos = getLocalPosition(system.obj, pos);
+        const planetPositions = getPlanetHelper(system.system);
+        const destroyedPlanets = getDestroyedPlanets(system.system);
 
-            const planetLocations = getPlanetLocations(
-                system.system,
-                system.x,
-                system.y,
-                system.rotation,
-                destroyedPlanets
-            );
+        const remainingPlanetPositions = planetPositions.filter(
+            (_element, index) => destroyedPlanets.indexOf(index) === -1
+        );
 
-            const onPlanet = planetLocations
-                .map((element) => withinCircle(element, pos))
-                .indexOf(true);
+        const onPlanet = remainingPlanetPositions
+            .map((element) => withinCircle(element, localPos))
+            .indexOf(true);
 
-            if (onPlanet > -1) {
-                if (debug) {
-                    // draw a sphere around the planet
-                    const planetLocation = planetLocations[onPlanet];
-                    const pos = new Vector(
-                        planetLocation.x,
-                        planetLocation.y,
-                        world.getTableHeight() + TILE_HEIGHT
-                    );
-                    world.drawDebugSphere(
-                        pos,
-                        planetLocation.r + RADIUS_BUFFER * SCALE,
-                        DEBUG_COLOR,
-                        DEBUG_DURATION,
-                        DEBUG_THICKNESS
-                    );
-                }
-                return currSystemPlanets[onPlanet];
+        if (onPlanet > -1) {
+            if (debug) {
+                const planetPos = remainingPlanetPositions[onPlanet];
+                const worldPos = getWorldPosition(system.obj, planetPos);
+
+                // convert the planet radius to world position
+                const planetRadius = (RADIUS_BUFFER + planetPos.r) * Hex.SCALE;
+                drawSphereAroundPlanet(worldPos, planetRadius);
             }
+            return system.system.planets[onPlanet];
         }
     }
 }
