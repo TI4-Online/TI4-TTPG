8abb9cc75f7c61a20401eec00b49324d62d2a30c
Christian Voshage <lonwyr@yahoo.de>
2022-02-11 19:16:53 +0100

    adding techology dummy UI

---
+++ src/objects/strategy-cards/technology.js
@@ -0,0 +1,47 @@
+const {
+    broadcastMessage,
+    onUiClosedClicked,
+    RegisterStrategyCardUI,
+} = require("./strategy-card");
+const {
+    Button,
+    Color,
+    Text,
+    VerticalBox,
+    refObject,
+} = require("../../wrapper/api");
+const locale = require("../../lib/locale");
+
+let selections = {};
+let activatingPlayer;
+
+
+function widgetFactory() {
+    let headerText = new Text()
+        .setFontSize(20)
+        .setText(locale("strategy_card.techology.text"));
+    let closeButton = new Button()
+        .setFontSize(10)
+        .setText(locale("strategy_card.base.button.close"));
+
+    closeButton.onClicked.add(onUiClosedClicked);
+
+    let verticalBox = new VerticalBox();
+    verticalBox.addChild(headerText);
+    verticalBox.addChild(closeButton);
+
+    return verticalBox;
+}
+
+const onStrategyCardPlayed = (card, player) => {};
+
+const onStrategyCardSelectionDone = (card, player) => {};
+
+new RegisterStrategyCardUI()
+    .setCard(refObject)
+    .setWidgetFactory(widgetFactory)
+    .setHeight(71)
+    .setColor(new Color(0.027, 0.203, 0.466))
+    .setOnStrategyCardPlayed(onStrategyCardPlayed)
+    .setOnStrategyCardSelectionDone(onStrategyCardSelectionDone)
+    .register();
