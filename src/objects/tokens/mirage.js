f007b30cffdaab1259d5b335d2e5f5c0972919f4
11qats <11qats@gmail.com>
2022-02-12 17:21:50 -0500

    adjust position/radius to be more accurate

--- src/objects/tokens/mirage.js
+++ src/objects/tokens/mirage.js
@@ -1,6 +1,7 @@
 const { refObject, Rotator } = require("../../wrapper/api");
 const { System, Planet } = require("../../lib/system/system");
 const { Broadcast } = require("../../lib/broadcast");
+const Vector = require("../../mock/mock-vector");
 
 const MIRAGE_ATTRS = {
     localeName: "planet.mirage",
@@ -8,6 +9,7 @@ const MIRAGE_ATTRS = {
     influence: 2,
     trait: ["cultural"],
     position: { x: 2, y: -1.25 },
+    radius: 1.75,
     legendary: true,
     legendaryCard: "card.legendary_planet:pok/mirage_flight_academy",
 };
@@ -51,17 +53,31 @@ function attachMirage(obj) {
         console.log("Attaching mirage to", system.tile);
         const mirage = new Planet(MIRAGE_ATTRS, system);
         system.planets.push(mirage);
+        _mirageSystemTileNumber = system.tile;
+
+        // place and lock the mirage token in the right location
+
+        // shift the yaw so that the token text is upright in the system
         const systemRot = systemObj.getRotation();
         const mirageRot = new Rotator(
             systemRot.pitch,
-            systemRot.yaw - 100,
+            systemRot.yaw - 110,
             systemRot.roll
         );
-        obj.setPosition(systemObj.localPositionToWorld(mirage.position));
+
+        // shift the position down slightly to account for the planet label
+        const scale = systemObj.getScale();
+        const shiftedMiragePos = new Vector(
+            mirage.position.x - 0.2 * scale.x,
+            mirage.position.y,
+            mirage.position.z
+        );
+        const miragePos = systemObj.localPositionToWorld(shiftedMiragePos);
+
+        obj.setPosition(miragePos);
         obj.setRotation(mirageRot);
-        obj.setScale(systemObj.getScale());
+        obj.setScale(scale);
         obj.toggleLock();
-        _mirageSystemTileNumber = system.tile;
     }
 }
 
