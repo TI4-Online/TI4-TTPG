26c5d269db20d898954855128f04f644ffbb6787
Christian Voshage <lonwyr@yahoo.de>
2022-02-22 22:37:18 +0100

    fixing technology code

--- src/objects/strategy-cards/technology.js
+++ src/objects/strategy-cards/technology.js
@@ -15,35 +15,36 @@ const { Technology } = require("../../lib/technology/technology");
 const locale = require("../../lib/locale");
 
 const imageSize = 30;
+const factionImageSize = 40;
 
 const factionIcons = {
-    arborec: "../../Textures/global/factions/arborec_icon.jpg",
-    argent: "../../Textures/global/factions/argent_icon.jpg",
-    cressus: "../../Textures/global/factions/cressus_icon.jpg",
-    empyrean: "../../Textures/global/factions/empyrean_icon.jpg",
-    hacan: "../../Textures/global/factions/hacan_icon.jpg",
-    jolnar: "../../Textures/global/factions/jolnar_icon.jpg",
-    l1z1x: "../../Textures/global/factions/l1z1x_icon.jpg",
-    letnev: "../../Textures/global/factions/letnev_icon.jpg",
-    mahact: "../../Textures/global/factions/mahact_icon.jpg",
-    mentak: "../../Textures/global/factions/mentak_icon.jpg",
-    muaat: "../../Textures/global/factions/arborec_icon.jpg",
-    naalu: "../../Textures/global/factions/naalu_icon.jpg",
-    naazrokha: "../../Textures/global/factions/naazrokha_icon.jpg",
-    nekro: "../../Textures/global/factions/nekro_icon.jpg",
-    nomad: "../../Textures/global/factions/nomad_icon.jpg",
-    norr: "../../Textures/global/factions/norr_icon.jpg",
-    saar: "../../Textures/global/factions/saar_icon.jpg",
-    sol: "../../Textures/global/factions/sol_icon.jpg",
-    ui: "../../Textures/global/factions/ul_icon.jpg",
-    vuilraith: "../../Textures/global/factions/vuilraith_icon.jpg",
-    winnu: "../../Textures/global/factions/winnu_icon.jpg",
-    xxcha: "../../Textures/global/factions/xxcha_icon.jpg",
-    yin: "../../Textures/global/factions/yin_icon.jpg",
-    yssaril: "../../Textures/global/factions/yssaril_icon.jpg",
+    arborec: "global/factions/arborec_icon.jpg",
+    argent: "global/factions/argent_icon.jpg",
+    creuss: "global/factions/creuss_icon.jpg",
+    empyrean: "global/factions/empyrean_icon.jpg",
+    hacan: "global/factions/hacan_icon.jpg",
+    jolnar: "global/factions/jolnar_icon.jpg",
+    l1z1x: "global/factions/l1z1x_icon.jpg",
+    letnev: "global/factions/letnev_icon.jpg",
+    mahact: "global/factions/mahact_icon.jpg",
+    mentak: "global/factions/mentak_icon.jpg",
+    muaat: "global/factions/muaat_icon.jpg",
+    naalu: "global/factions/naalu_icon.jpg",
+    naazrokha: "global/factions/naazrokha_icon.jpg",
+    nekro: "global/factions/nekro_icon.jpg",
+    nomad: "global/factions/nomad_icon.jpg",
+    norr: "global/factions/norr_icon.jpg",
+    saar: "global/factions/saar_icon.jpg",
+    sol: "global/factions/sol_icon.jpg",
+    ui: "global/factions/ul_icon.jpg",
+    vuilraith: "global/factions/vuilraith_icon.jpg",
+    winnu: "global/factions/winnu_icon.jpg",
+    xxcha: "global/factions/xxcha_icon.jpg",
+    yin: "global/factions/yin_icon.jpg",
+    yssaril: "global/factions/yssaril_icon.jpg",
 };
 
-const TechIcons = {
+const techIcons = {
     unitUpgrade: {
         color: new Color(1, 1, 1),
     },
@@ -77,14 +78,23 @@ const TechIcons = {
     },
 };
 
-function drawTechButton(canvas, xOffset, yOffset, tech, playerTechnologies, packageId) {
+function drawTechButton(canvas, xOffset, yOffset, tech, playerTechnologies, ownedTechnologies, packageId) {
     let techButton = new Button()
         .setText(tech.name)
-        .setTextColor(techIcons[tech.type].color);
+        .setTextColor(techIcons[tech.type].color)
+        .setEnabled(!ownedTechnologies.includes(tech));
+    techButton.onClicked.add(onTechResearched);
     canvas.addChild(techButton, xOffset, yOffset, 200, 35);
+    
+    if (tech.faction) {
+        let factionIcon = new ImageWidget()
+        .setImage(factionIcons[tech.faction], packageId)
+        .setImageSize(imageSize, imageSize);
+        canvas.addChild(factionIcon, xOffset + 160, yOffset + 23, imageSize, imageSize);
+    }
 
     let numOfIcons = 0;
-
+    
     if (Object.keys(tech.requirements).length > 0) {
         for (let requirement in tech.requirements) {
             for (let i = 0; i < tech.requirements[requirement]; i++) {
@@ -109,7 +119,7 @@ function drawTechButton(canvas, xOffset, yOffset, tech, playerTechnologies, pack
 }
 
 const countPlayerTechsByType = (playerSlot) => {
-    const playerTechnologies = {
+    let playerTechnologies = {
         Blue: 0,
         Red: 0,
         Yellow: 0,
@@ -137,8 +147,9 @@ const onTechResearched = (button, player) => {
     const skippedTechs = {};
 
     for (let requirement in technology.requirements) {
-        const required = technology[requirement];
+        const required = technology.requirements[requirement];
         const owned = ownedTechnologies[requirement];
+
         if (required > owned) {
             skippedTechs[requirement] = required - owned;
         }
@@ -148,11 +159,11 @@ const onTechResearched = (button, player) => {
     let messageParameters = {
         playerName: player.getName(),
         technologyName: technologyName,
+        skips: ""
     };
 
     if (Object.keys(skippedTechs).length) {
         messageKey = "strategy_card.technology.message.researched_and_skips";
-        messageParameters.skips = "";
         for (let requirement in skippedTechs) {
             if (messageParameters.skips) {
                 messageParameters.skips += ", ";
@@ -160,8 +171,9 @@ const onTechResearched = (button, player) => {
 
             const techType = locale(`technology.type.${requirement}`);
 
-            messageParameters.skip += `${skippedTechs[requirement]} ${techType}`;
+            messageParameters.skips += `${skippedTechs[requirement]} ${techType}`;
         }
+        console.log(`skippedTechs: ${JSON.stringify(skippedTechs)} - skips: ${messageParameters.skips}`);
     }
 
     broadcastMessage(messageKey, messageParameters, player);
@@ -169,7 +181,7 @@ const onTechResearched = (button, player) => {
 
 function widgetFactory(playerDesk, packageId) {
     const playerSlot = playerDesk.playerSlot;
-    const technologies = Technology.getTechnologiesByType(playerSlot);
+    const technologies = Technology.getTechnologiesByType(playerDesk.playerSlot);
     const ownedTechnologies = Technology.getOwnedPlayerTechnologies(playerSlot);
     const playerTechnologies = countPlayerTechsByType(playerSlot);
     let xOffset = 0;
@@ -182,23 +194,18 @@ function widgetFactory(playerDesk, packageId) {
         .setText(locale("strategy_card.technology.text"));
     canvas.addChild(headerText, 0, 0, 200, 35);
 
-    const playerTechnologies = {
-        Blue: 0,
-        Red: 1,
-        Yellow: 2,
-        Green: 3,
-    };
-
-    const technologies = Technology.getTechnologiesByType(
-        playerDesk.playerSlot
-    );
-
     ["Blue", "Red", "Yellow", "Green"].forEach((type) => {
         let yOffset = 50;
         technologies[type].forEach((tech) => {
+            drawTechButton(canvas, xOffset, yOffset, tech, playerTechnologies, ownedTechnologies, packageId);
+
+            if (Object.keys(tech.requirements).length > 0) {
+                yOffset += 15;
+            }
+/*
             let techButton = new Button()
                 .setText(tech.name)
-                .setTextColor(TechIcons[type].color)
+                .setTextColor(techIcons[type].color)
                 .setEnabled(!ownedTechnologies.includes(tech))
                 .onClicked.add(onTechResearched);
             canvas.addChild(techButton, xOffset, yOffset, 200, 35);
@@ -216,8 +223,8 @@ function widgetFactory(playerDesk, packageId) {
                     for (let i = 0; i < tech.requirements[requirement]; i++) {
                         const image =
                             playerTechnologies[requirement] > i
-                                ? TechIcons[requirement].activeIcon
-                                : TechIcons[requirement].disabledIcon;
+                                ? techIcons[requirement].activeIcon
+                                : techIcons[requirement].disabledIcon;
                         let techIcon = new ImageWidget().setImage(image);
                         canvas.addChild(
                             techIcon,
@@ -228,7 +235,7 @@ function widgetFactory(playerDesk, packageId) {
                         );
                     }
                 }
-            }
+            }*/
             yOffset += 40;
         });
         yOffsetMax = Math.max(yOffset, yOffsetMax);
@@ -241,14 +248,14 @@ function widgetFactory(playerDesk, packageId) {
         const yOffset = yOffsetMax + 20 + Math.floor(index / 4) * 60;
         canvas.addChild(techButton, xOffset, yOffset, 200, 35);
 
-        drawTechButton(canvas, xOffset, yOffset, tech, playerTechnologies, packageId);
+        drawTechButton(canvas, xOffset, yOffset, tech, playerTechnologies, ownedTechnologies, packageId);
     });
 
     let closeButton = new Button()
         .setFontSize(10)
         .setText(locale("strategy_card.base.button.close"));
     closeButton.onClicked.add(onUiClosedClicked);
-    canvas.addChild(closeButton, 0, calculateHeight(playerDesk.playerSlot) - 45, 838, 48);
+    canvas.addChild(closeButton, 0, calculateHeight(playerDesk.playerSlot) - 45, 830, 48);
 
     return canvas;
 }
@@ -266,6 +273,6 @@ new RegisterStrategyCardUI()
     .setCard(refObject)
     .setWidgetFactory(widgetFactory)
     .setHeight(calculateHeight)
-    .setWidth(840)
+    .setWidth(833)
     .setColor(new Color(0.027, 0.203, 0.466))
     .register();
