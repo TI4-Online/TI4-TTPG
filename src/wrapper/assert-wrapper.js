6b3306f38cd6db9a55b697ce66fbb2ddf17bb9f9
Darrell <darrell.anderson@gmail.com>
2022-02-26 11:36:19 -0800

    commit to using an assert wrapper (node module bugged)

--- src/wrapper/assert-wrapper.js
+++ src/wrapper/assert-wrapper.js
@@ -1,19 +1,9 @@
-// Temporary workaround for TTPG/MacOS require node_modules bug.
-// Remove this when TTPG fixes it, fix requires to use real assert.
+// The node modules "assert" is broken.  Use this instead.
 // Usage: const assert = require('./wrapper/assert')
-
-let assert = false;
-try {
-    //assert = require("assert");
-} catch {
-    assert = false;
-}
-if (!assert) {
-    assert = (test, message) => {
-        if (!test) {
-            //console.log(new Error().stack);
-            throw new Error(message ? message : "assertion failed");
-        }
-    };
-}
-module.exports = assert;
+module.exports = (test, message) => {
+    if (!test) {
+        //console.log(new Error().stack);
+        throw new Error(message ? message : "assertion failed");
+    }
+    return test;
+};
