7e02ac844cf922272dca6576541d9f078282d36b
11qats <11qats@gmail.com>
2022-02-27 13:39:33 -0500

    use Broadcast and locale to report

--- src/objects/containers/command-token-container.js
+++ src/objects/containers/command-token-container.js
@@ -5,6 +5,8 @@ const {
     REJECT_REASON,
 } = require("../../global/patch-exclusive-bags");
 const { ObjectNamespace } = require("../../lib/object-namespace");
+const { Broadcast } = require("../../lib/broadcast");
+const locale = require("../../lib/locale");
 
 const DELAY = 3000; // delay in milliseconds
 
@@ -21,10 +23,23 @@ function holdsObject(container, object) {
     return !rejectReason;
 }
 
+function getFactionName(object) {
+    // cant get the faction name from the container because the container has name "*"
+    // therefore get it from the command token object
+    assert(ObjectNamespace.isCommandToken(object));
+    const factionName = ObjectNamespace.parseCommandToken(object).name;
+    return locale("faction.full." + factionName);
+}
+
 class Reporter {
     constructor(container) {
         assert(container instanceof Container);
         this._container = container;
+
+        this._factionName = locale(
+            "faction.full." +
+                ObjectNamespace.parseTokenBag(this._container).name
+        );
 
         // arrow functions necessary to get proper "this" value
         this._container.onInserted.add((container, objects, player) =>
@@ -41,6 +56,9 @@ class Reporter {
         this._firstRemoved = false;
     }
 
+    // TODO: broadcast message when a script add token to the bag
+    // e.g. when command token is dropped in the owner token bag and then
+    // moved to the command token bag
     countInserted(container, objects, _player) {
         // ensure each inserted object belongs in the container before counting
         // it toward the number of inserted objects
@@ -55,7 +73,12 @@ class Reporter {
         if (addedValidObject && !this._firstInserted) {
             this._firstInserted = true;
             setTimeout(() => {
-                console.log(`${this._insertedCounter} tokens returned`);
+                Broadcast.chatAll(
+                    locale("ui.message.command_tokens_inserted", {
+                        factionName: getFactionName(objects[0]),
+                        count: this._insertedCounter,
+                    })
+                );
                 this._insertedCounter = 0;
                 this._firstInserted = false;
             }, DELAY);
@@ -72,7 +95,12 @@ class Reporter {
         if (!this._firstRemoved) {
             this._firstRemoved = true;
             setTimeout(() => {
-                console.log(`${this._removedCounter} tokens taken`);
+                Broadcast.chatAll(
+                    locale("ui.message.command_tokens_removed", {
+                        factionName: getFactionName(object),
+                        count: this._removedCounter,
+                    })
+                );
                 this._removedCounter = 0;
                 this._firstRemoved = false;
             }, DELAY);
