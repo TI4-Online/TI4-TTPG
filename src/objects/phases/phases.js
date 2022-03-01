2214b6c170e3b12cc50cda11a280041163cb7107
Darrell <darrell.anderson@gmail.com>
2022-02-28 16:44:04 -0800

    use map-tool, auto-roller as tabs

--- src/objects/phases/phases.js
+++ src/objects/phases/phases.js
@@ -2,54 +2,62 @@ const assert = require("../../wrapper/assert-wrapper");
 const locale = require("../../lib/locale");
 const { TabbedPanel } = require("../../lib/ui/tabbed-panel");
 const { TabStrategyUI } = require("./tab-strategy-ui");
+const { TabActionUI } = require("./tab-action-ui");
 const {
     GameObject,
+    LayoutBox,
     Text,
     UIElement,
     Vector,
     refObject,
     world,
 } = require("../../wrapper/api");
-const { TabActionUI } = require("./tab-action-ui");
+const { MapTool } = require("../map-tool/map-tool");
+const { AutoRoller } = require("../roller/auto-roller");
 
 class Phases {
     constructor(gameObject) {
         assert(gameObject instanceof GameObject);
-        this._obj = gameObject;
 
-        this._tabbedPanel = new TabbedPanel();
+        const mapTool = new MapTool();
+        const autoRoller = new AutoRoller();
 
-        this._tabbedPanel.addTab(
-            locale("ui.phase.strategy.label"),
-            new TabStrategyUI()
-        );
-        this._tabbedPanel.addTab(
-            locale("ui.phase.action.label"),
-            new TabActionUI()
-        );
-        this._tabbedPanel.addTab(
-            locale("ui.phase.status.label"),
-            new Text().setText("status here")
-        );
-        this._tabbedPanel.addTab(
-            locale("ui.phase.agenda.label"),
-            new Text().setText("agenda here")
-        );
+        const tabbedPanel = new TabbedPanel()
+            .addTab("ui.tab.map_tool", mapTool.getUI())
+            .addTab(locale("ui.phase.strategy.label"), new TabStrategyUI())
+            .addTab(locale("ui.tab.auto_roller"), autoRoller.getUI())
+            .addTab(
+                locale("ui.phase.status.label"),
+                new Text().setText("status here")
+            )
+            .addTab(
+                locale("ui.phase.agenda.label"),
+                new Text().setText("agenda here")
+            );
 
-        this._uiElement = new UIElement();
-        this._uiElement.position = new Vector(0, 0, 5);
-        this._uiElement.useWidgetSize = false;
-        this._uiElement.width = 500;
-        this._uiElement.height = 500;
-        this._uiElement.widget = this._tabbedPanel;
-        this._obj.addUI(this._uiElement);
+        const w = 450;
+        const layoutBox = new LayoutBox()
+            .setChild(tabbedPanel)
+            .setMaximumWidth(w)
+            .setMinimumWidth(w);
+
+        const uiElement = new UIElement();
+        uiElement.anchorY = 0;
+        uiElement.position = new Vector(0, 0, 5);
+        uiElement.widget = layoutBox;
+        gameObject.addUI(uiElement);
+
+        mapTool.getUI().setOwningObjectForUpdate(gameObject, uiElement);
+        autoRoller.getUI().setOwningObjectForUpdate(gameObject, uiElement);
     }
 }
 
 refObject.onCreated.add((obj) => {
+    console.log("Phases onCreated");
     new Phases(obj);
 });
 
 if (world.getExecutionReason() === "ScriptReload") {
+    console.log("Phases ScriptReload");
     new Phases(refObject);
 }
