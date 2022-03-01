12bbfb8bc08ab1665e9e80585aa14b802ec28bdb
Darrell <darrell.anderson@gmail.com>
2022-02-28 16:43:15 -0800

    better tabbed ui, make setChild return self for chaining

--- src/lib/ui/tabbed-panel.js
+++ src/lib/ui/tabbed-panel.js
@@ -21,31 +21,54 @@ class TabbedPanel extends Border {
         super.setChild(this._verticalBox);
 
         // First element is tabs.
-        this._tabs = new HorizontalBox();
-        this._verticalBox.addChild(this._tabs);
+        this._tabButtons = new HorizontalBox();
+        this._verticalBox.addChild(this._tabButtons);
+
+        this._labelToTabData = {};
+    }
+
+    _selectTab(label) {
+        console.log(`TabbedPanel._selectTab("${label}")`);
+        const tab = this._labelToTabData[label];
+        assert(tab);
+
+        // Enable other tabs.
+        for (const tabData of Object.values(this._labelToTabData)) {
+            tabData.tabButton.setEnabled(tabData.label !== label);
+        }
+
+        // Show tab content.
+        if (this._verticalBox.getChildAt(1)) {
+            this._verticalBox.removeChildAt(1);
+        }
+        assert(tab.widget);
+        this._verticalBox.addChild(tab.widget);
     }
 
     addTab(label, widget) {
-        assert(typeof label === "string");
+        assert(typeof label === "string" && label.length > 0);
         assert(widget instanceof Widget);
 
         const tabButton = new Button().setText(label);
         tabButton.onClicked.add((button, player) => {
-            // TODO XXX disable tab for new content, enable others.
-
-            // Show tab content.
-            if (this._verticalBox.getChildAt(1)) {
-                this._verticalBox.removeChildAt(1);
-            }
-            this._verticalBox.addChild(widget);
+            this._selectTab(label);
         });
 
         const weight = 1; // make all same width
-        this._tabs.addChild(tabButton, weight);
+        this._tabButtons.addChild(tabButton, weight);
+
+        if (this._labelToTabData[label]) {
+            throw new Error(`already have tab "${label}"`);
+        }
+        this._labelToTabData[label] = {
+            label,
+            tabButton,
+            widget,
+        };
 
         // If this is the first tab, set it visible.
         if (!this._verticalBox.getChildAt(1)) {
-            this._verticalBox.addChild(widget);
+            this._selectTab(label);
         }
 
         return this;
