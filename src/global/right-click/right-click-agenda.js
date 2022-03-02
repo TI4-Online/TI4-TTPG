a6e6db5d0ec7f18791b61e59e5ed0284192eeed1
Darrell <darrell.anderson@gmail.com>
2022-03-01 22:18:18 -0800

    right click agenda card top/bottom

---
+++ src/global/right-click/right-click-agenda.js
@@ -0,0 +1,69 @@
+const assert = require("../wrapper/assert-wrapper");
+const locale = require("../lib/locale");
+const { DealDiscard } = require("../../lib/card/deal-discard");
+const { ObjectNamespace } = require("../../lib/object-namespace");
+const { Card, globalEvents, world } = require("../wrapper/api");
+
+function _placeAgenda(agendaCard, onTop) {
+    assert(agendaCard instanceof Card);
+    assert(typeof onTop === "boolean");
+
+    const deck = DealDiscard.getDeckWithReshuffle("card.agenda");
+    if (!deck) {
+        return;
+    }
+    assert(deck instanceof Card);
+    const toFront = !onTop;
+    const offset = 0;
+    const animate = true;
+    const flipped = false;
+    deck.addCards(agendaCard, toFront, offset, animate, flipped);
+}
+
+function addRightClickOptions(agendaCard) {
+    assert(agendaCard instanceof Card);
+    const namesAndActions = [
+        {
+            name: locale("ui.menu.place_agenda_top"),
+            action: (player) => {
+                _placeAgenda(agendaCard, true);
+            },
+        },
+        {
+            name: locale("ui.menu.place_agenda_bottom"),
+            action: (player) => {
+                _placeAgenda(agendaCard, false);
+            },
+        },
+    ];
+
+    // Add as right-click options.
+    for (const nameAndAction of namesAndActions) {
+        agendaCard.addCustomAction("*" + nameAndAction.name);
+    }
+    agendaCard.onCustomAction.add((obj, player, actionName) => {
+        for (const nameAndAction of namesAndActions) {
+            if ("*" + nameAndAction.name === actionName) {
+                nameAndAction.action(player);
+                break;
+            }
+        }
+    });
+}
+
+globalEvents.onObjectCreated.add((obj) => {
+    const nsid = ObjectNamespace.getNsid(obj);
+    if (nsid.startsWith("card.agenda")) {
+        addRightClickOptions(obj);
+    }
+});
+
+// Script reload doesn't call onObjectCreated on existing objects, load manually.
+if (world.getExecutionReason() === "ScriptReload") {
+    for (const obj of world.getAllObjects()) {
+        const nsid = ObjectNamespace.getNsid(obj);
+        if (nsid.startsWith("card.agenda")) {
+            addRightClickOptions(obj);
+        }
+    }
+}
