8f57803798f55ad16b9c5be21d24648c37a72331
Darrell <darrell.anderson@gmail.com>
2022-03-05 22:38:53 -0800

    log assert stack trace if not a test, some contexts do not show it normally

--- src/wrapper/assert-wrapper.js
+++ src/wrapper/assert-wrapper.js
@@ -1,8 +1,14 @@
+const { world } = require("./api");
+
 // The node modules "assert" is broken.  Use this instead.
 // Usage: const assert = require('./wrapper/assert')
 module.exports = (test, message) => {
     if (!test) {
-        //console.log(new Error().stack);
+        if (!world.__isMock) {
+            // Come contexts do not print out a stack trace.
+            // Do it manually.
+            console.log(new Error().stack);
+        }
         throw new Error(message ? message : "assertion failed");
     }
     return test;
