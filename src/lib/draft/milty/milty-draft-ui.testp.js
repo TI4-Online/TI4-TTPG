08d6e6ea7639caa645270f507eadd0a2654ef032
Darrell <darrell.anderson@gmail.com>
2022-03-07 21:28:03 -0800

    put a background on draft testp

--- src/lib/draft/milty/milty-draft-ui.testp.js
+++ src/lib/draft/milty/milty-draft-ui.testp.js
@@ -1,12 +1,17 @@
 const { DEFAULT_SLICE_SCALE } = require("./milty-slice-ui");
 const { MiltyDraftUI } = require("./milty-draft-ui");
-const { UIElement, Vector, refObject } = require("../../../wrapper/api");
+const {
+    Border,
+    UIElement,
+    Vector,
+    refObject,
+} = require("../../../wrapper/api");
 
 const miltyDraftWidget = new MiltyDraftUI();
 
 const ui = new UIElement();
 ui.position = new Vector(0, 0, 3);
-ui.widget = miltyDraftWidget;
+ui.widget = new Border().setColor([0.5, 0.5, 0.5]).setChild(miltyDraftWidget);
 ui.scale = 1 / DEFAULT_SLICE_SCALE;
 
 refObject.addUI(ui);
