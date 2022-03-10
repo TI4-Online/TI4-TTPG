a66ffc3fa86099eda3f63b253252ce6f2c037e2e
Darrell <darrell.anderson@gmail.com>
2022-03-10 13:09:08 -0800

    get player desk info from table layout

--- src/lib/player-desk/player-desk.js
+++ src/lib/player-desk/player-desk.js
@@ -4,6 +4,7 @@ const { ColorUtil } = require("../color/color-util");
 const { ObjectNamespace } = require("../object-namespace");
 const { PlayerDeskSetup } = require("./player-desk-setup");
 const { PlayerDeskUI } = require("./player-desk-ui");
+const { TableLayout } = require("../../table/table-layout");
 const {
     GlobalSavedData,
     GLOBAL_SAVED_DATA_KEY,
@@ -17,89 +18,6 @@ const {
     globalEvents,
     world,
 } = require("../../wrapper/api");
-
-/**
- * Desk positions in cm and rotation in degrees.  Z ignored.
- * First is "right", the counterclockwise about the table.
- */
-const PLAYER_DESKS = [
-    // East
-    {
-        colorName: "pink",
-        hexColor: "#FF84D6",
-        plasticHexColor: "#F46FCD",
-        pos: { x: 0, y: 160 },
-        yaw: 180.0,
-        defaultPlayerSlot: 5,
-        playerCounts: [7, 8],
-    },
-    // North (right to left)
-    {
-        colorName: "green",
-        hexColor: "#5AE35A",
-        plasticHexColor: "#00A306",
-        pos: { x: 102.4, y: 100.8 },
-        yaw: 120,
-        defaultPlayerSlot: 1,
-        playerCounts: [3, 4, 5, 6, 7, 8],
-    },
-    {
-        colorName: "red",
-        hexColor: "#FF2417",
-        plasticHexColor: "#CB0000",
-        pos: { x: 129.4, y: 0 },
-        yaw: 90.0,
-        defaultPlayerSlot: 16,
-        playerCounts: [2, 5, 6, 7, 8],
-    },
-    {
-        colorName: "yellow",
-        hexColor: "#FFDA00",
-        plasticHexColor: "#FFDA00",
-        pos: { x: 102.4, y: -100.8 },
-        yaw: 60,
-        defaultPlayerSlot: 9,
-        playerCounts: [3, 4, 5, 6, 7, 8],
-    },
-    // West
-    {
-        colorName: "orange",
-        hexColor: "#FF932B",
-        plasticHexColor: "#FF7603",
-        pos: { x: 0, y: -160 },
-        yaw: 0,
-        defaultPlayerSlot: 6,
-        playerCounts: [8],
-    },
-    // South (left to right)
-    {
-        colorName: "purple",
-        hexColor: "#C800FF",
-        plasticHexColor: "#5E219C",
-        pos: { x: -102.4, y: -100.8 },
-        yaw: -60,
-        defaultPlayerSlot: 4,
-        playerCounts: [4, 5, 6, 7, 8],
-    },
-    {
-        colorName: "blue",
-        hexColor: "#07B2FF",
-        plasticHexColor: "#07B2FF",
-        pos: { x: -129.4, y: 0 },
-        yaw: -90.0,
-        defaultPlayerSlot: 15,
-        playerCounts: [1, 2, 3, 6, 7, 8],
-    },
-    {
-        colorName: "white",
-        hexColor: "#BABABA",
-        plasticHexColor: "#C1C1C1",
-        pos: { x: -102.4, y: 100.8 },
-        yaw: -120,
-        defaultPlayerSlot: 18,
-        playerCounts: [4, 5, 6, 7, 8],
-    },
-];
 
 const SEAT_CAMERA = {
     pos: { x: -90, y: -6, z: 100 },
@@ -221,8 +139,9 @@ class PlayerDesk {
         const playerCount = world.TI4.config.playerCount;
         _playerDesks = [];
         // Walk backwards so "south-east" is index 0 then clockwise.
-        for (let i = PLAYER_DESKS.length - 1; i >= 0; i--) {
-            const attrs = PLAYER_DESKS[i];
+        const tableDesks = TableLayout.desks();
+        for (let i = tableDesks.length - 1; i >= 0; i--) {
+            const attrs = tableDesks[i];
             if (!attrs.playerCounts.includes(playerCount)) {
                 continue;
             }
@@ -514,16 +433,13 @@ class PlayerDesk {
      * @returns {Array.{colorName:string,color:Color,plasticColor:Color}}
      */
     getColorOptions() {
-        const result = [];
-        for (let i = 0; i < PLAYER_DESKS.length; i++) {
-            const attrs = PLAYER_DESKS[i];
-            result.push({
+        return TableLayout.desks().map((attrs) => {
+            return {
                 colorName: attrs.colorName,
                 colorTint: ColorUtil.colorFromHex(attrs.hexColor),
                 plasticColorTint: ColorUtil.colorFromHex(attrs.plasticHexColor),
-            });
-        }
-        return result;
+            };
+        });
     }
 
     /**
@@ -540,8 +456,10 @@ class PlayerDesk {
         assert(ColorUtil.isColor(colorTint));
         assert(ColorUtil.isColor(plasticColorTint));
 
+        const tableDesks = TableLayout.desks();
+
         let legalColorName = false;
-        for (const deskAttrs of PLAYER_DESKS) {
+        for (const deskAttrs of tableDesks) {
             if (deskAttrs.colorName === colorName) {
                 legalColorName = true;
                 break;
@@ -576,7 +494,7 @@ class PlayerDesk {
             dstFaction = world.TI4.getFactionByPlayerSlot(dstPlayerSlot);
         } else {
             // Not swapping with an active seat.  Lookup from unused seats.
-            for (const deskAttrs of PLAYER_DESKS) {
+            for (const deskAttrs of tableDesks) {
                 if (deskAttrs.colorName === colorName) {
                     dstPlayerSlot = deskAttrs.defaultPlayerSlot;
                     break;
