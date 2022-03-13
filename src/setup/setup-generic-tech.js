1a326070f9b0b04fc760ce80989079654b147ded
Darrell <darrell.anderson@gmail.com>
2022-03-13 12:12:16 -0700

    build area (placeholder obj)

--- src/setup/setup-generic-tech.js
+++ src/setup/setup-generic-tech.js
@@ -3,7 +3,7 @@ const { AbstractSetup } = require("./abstract-setup");
 const { CardUtil } = require("../lib/card/card-util");
 const { Vector, world } = require("../wrapper/api");
 
-const TECH_DECK_LOCAL_OFFSET = { x: 2, y: -3 };
+const TECH_DECK_LOCAL_OFFSET = { x: 2, y: 7 };
 
 class SetupGenericTech extends AbstractSetup {
     constructor(playerDesk) {
