90da73dbde3b3019ed88b7bdff70d49ed0948a22
Darrell <darrell.anderson@gmail.com>
2022-03-16 16:17:53 -0700

    tweak tactical action ui to better fit with game ui

--- src/objects/roller/auto-roller-ui.js
+++ src/objects/roller/auto-roller-ui.js
@@ -1,18 +1,22 @@
 const assert = require("../../wrapper/assert-wrapper");
 const locale = require("../../lib/locale");
-const { System } = require("../../lib/system/system");
+const { System, Planet } = require("../../lib/system/system");
 const CONFIG = require("../../game-ui/game-ui-config");
 const {
     Button,
     GameObject,
     HorizontalBox,
     LayoutBox,
+    Panel,
     Text,
     TextJustification,
     UIElement,
+    VerticalAlignment,
     VerticalBox,
     refPackageId,
+    world,
 } = require("../../wrapper/api");
+const { Border } = require("@tabletop-playground/api");
 
 /**
  * Manage the UI on an AutoRoller object.
@@ -31,7 +35,8 @@ class AutoRollerUI extends LayoutBox {
         this._onButton = onButton;
         this._player = undefined;
 
-        this.resetAwaitingSystemActivation();
+        //this.resetAwaitingSystemActivation();
+        this.resetAfterSystemActivation(world.TI4.getSystemByTileNumber(16));
     }
 
     setOwningObjectForUpdate(gameObject, uiElement) {
@@ -94,249 +99,125 @@ class AutoRollerUI extends LayoutBox {
     resetAfterSystemActivation(system) {
         assert(system instanceof System);
 
-        const panels = [new VerticalBox().setChildDistance(5)];
+        // Mandate column width so if button text overflows it truncates
+        // instead of adding a scrollbar.  Do not assume EN locale!
+        const VERTICAL_DISTANCE = 5;
+        const HORIZONTAL_DISTANCE = 10;
+        const COLUMN_WIDTH = 175;
 
-        const addButton = (parent, localeText, combatType, planet) => {
-            assert(typeof parent === "object");
+        const spaceLayout = new VerticalBox().setChildDistance(
+            VERTICAL_DISTANCE
+        );
+        const spaceBox = new LayoutBox()
+            .setOverrideWidth(COLUMN_WIDTH)
+            .setChild(spaceLayout);
+
+        const groundLayout = new HorizontalBox().setChildDistance(
+            HORIZONTAL_DISTANCE
+        );
+        const groundBox = new LayoutBox()
+            .setOverrideWidth(COLUMN_WIDTH * 3 + HORIZONTAL_DISTANCE * 3)
+            .setVerticalAlignment(VerticalAlignment.Bottom)
+            .setPadding(0, 0, 0, 44)
+            .setChild(groundLayout);
+
+        const BUTTON_FONT_SIZE = 12;
+        const LABEL_FONT = "Handel_Gothic_Regular.otf";
+        const LABEL_FONT_SIZE = 14;
+
+        this.setChild(
+            new HorizontalBox()
+                .setChildDistance(HORIZONTAL_DISTANCE)
+                .addChild(spaceBox)
+                .addChild(new Border().setColor(CONFIG.spacerColor))
+                .addChild(groundBox)
+        );
+
+        let panel = spaceLayout;
+
+        const addStep = (localeText) => {
+            assert(panel instanceof Panel);
+            assert(typeof localeText === "string");
+            const label = new Text()
+                .setFontSize(LABEL_FONT_SIZE)
+                .setFont(LABEL_FONT, refPackageId)
+                .setJustification(TextJustification.Center)
+                .setText(locale(localeText).toUpperCase());
+            panel.addChild(label);
+        };
+
+        const addPlanetName = (planet) => {
+            assert(panel instanceof Panel);
+            assert(planet instanceof Planet);
+            const label = new Text()
+                .setFontSize(LABEL_FONT_SIZE)
+                .setJustification(TextJustification.Center)
+                .setItalic(true)
+                .setText(planet.getNameStr());
+            panel.addChild(label);
+        };
+
+        const addButton = (localeText, combatType, planet) => {
+            assert(panel instanceof Panel);
+            assert(typeof localeText === "string");
+            assert(typeof combatType === "string");
+            assert(!planet || planet instanceof Planet);
             const button = new Button()
-                .setText(locale(localeText))
-                .setFontSize(9);
+                .setFontSize(BUTTON_FONT_SIZE)
+                .setText(locale(localeText));
             button.onClicked.add((button, player) => {
                 this._onButton(combatType, planet, player);
             });
-            parent[parent.length - 1].addChild(button);
+            panel.addChild(button);
             return button;
         };
 
-        const addLayoutButton = (
-            parent,
-            localeText,
-            combatType,
-            planet,
-            fontSize,
-            maxWidth,
-            maxHeight
-        ) => {
-            assert(typeof parent === "object");
-            const lbox1 = new LayoutBox()
-                .setOverrideWidth(maxWidth)
-                .setOverrideHeight(maxHeight);
-            const button = new Button()
-                .setText(locale(localeText))
-                .setFontSize(fontSize);
-            const lbox2 = new LayoutBox()
-                .setVerticalAlignment(2)
-                .setHorizontalAlignment(2);
-            const hpanel = new HorizontalBox().setChildDistance(5);
-            button.onClicked.add((button, player) => {
-                this._onButton(combatType, planet, player);
-            });
-            lbox1.setChild(button);
-            hpanel.addChild(lbox1);
-            lbox2.setChild(hpanel);
-            parent[parent.length - 1].addChild(lbox2);
-        };
-
-        const addLayoutText = (
-            parent,
-            localeText,
-            fontSize,
-            italize,
-            maxWidth,
-            maxHeight,
-            fontfile = ""
-        ) => {
-            assert(typeof parent === typeof []);
-            const text = new Text()
-                .setText(locale(localeText))
-                .setJustification(TextJustification.Center)
-                .setFontSize(fontSize)
-                .setItalic(italize)
-                .setFont(fontfile, refPackageId);
-            const lbox1 = new LayoutBox()
-                .setOverrideWidth(maxWidth)
-                .setOverrideHeight(maxHeight);
-            const lbox2 = new LayoutBox()
-                .setVerticalAlignment(2)
-                .setHorizontalAlignment(2);
-            const hpanel = new HorizontalBox().setChildDistance(5);
-            lbox1.setChild(text);
-            hpanel.addChild(lbox1);
-            lbox2.setChild(hpanel);
-            parent[parent.length - 1].addChild(lbox2);
-        };
-
-        const addHorizontalSubPanel = (parent, spacing) => {
-            assert(typeof parent === typeof []);
-            const panel = new HorizontalBox().setChildDistance(spacing);
-            const lbox = new LayoutBox()
-                .setVerticalAlignment(2)
-                .setHorizontalAlignment(2);
-            lbox.setChild(panel);
-            parent[parent.length - 1].addChild(lbox);
-            parent.push(panel);
-        };
-
-        const addVerticalSubPanel = (parent, spacing) => {
-            assert(typeof parent === typeof []);
-            const panel = new VerticalBox().setChildDistance(spacing);
-            parent[parent.length - 1].addChild(panel);
-            parent.push(panel);
-        };
-
-        const addPlanetSteps = (parent, planet) => {
-            assert(typeof parent === typeof []);
-            addVerticalSubPanel(parent, 5);
-            addLayoutText(parent, planet.localeName, 10, true, -1, -1);
-            addButton(
-                parent,
-                locale("ui.roller.bombardment"),
-                "bombardment",
-                planet
+        const addGap = () => {
+            assert(panel instanceof Panel);
+            panel.addChild(
+                new LayoutBox().setOverrideHeight(VERTICAL_DISTANCE)
             );
+        };
+
+        // MOVEMENT STEP
+        addStep("ui.label.movement");
+        addButton("ui.movement.finish_movement", "finishMove");
+        addButton("ui.roller.space_cannon_offense", "spaceCannon");
+        addGap();
+
+        // SPACE COMBAT STEP
+        addStep("ui.label.space_combat");
+        addButton("ui.roller.anti_fighter_barrage", "antiFighterBarrage");
+        addButton("ui.roller.announce_retreat", "announceRetreat");
+        addButton("ui.roller.space_combat", "spaceCombat");
+        addGap();
+
+        // INVASION (redirect)
+        addStep("ui.label.invasion");
+        addButton("—>", "").setEnabled(false);
+        addGap();
+
+        // PRODUCTION
+        addStep("ui.label.production");
+        addButton("ui.label.production", "production");
+
+        // INVASION (real this time)
+        for (const planet of system.planets) {
+            panel = new VerticalBox().setChildDistance(VERTICAL_DISTANCE);
+            groundLayout.addChild(
+                new LayoutBox().setOverrideWidth(COLUMN_WIDTH).setChild(panel)
+            );
+
+            addPlanetName(planet);
+            addButton("ui.roller.bombardment", "bombardment", planet);
             addButton(
-                parent,
-                locale("ui.roller.space_cannon_defense"),
+                "ui.roller.space_cannon_defense",
                 "spaceCannonDefense",
                 planet
             );
-            addButton(
-                parent,
-                locale("ui.roller.ground_combat"),
-                "groundCombat",
-                planet
-            );
-            parent.pop();
-        };
+            addButton("ui.roller.ground_combat", "groundCombat", planet);
+        }
 
-        const addGap = (parent, padW, padE, padN, padS) => {
-            assert(typeof parent === typeof []);
-            const lbox = new LayoutBox();
-            lbox.setPadding(padW, padE, padN, padS);
-            parent[parent.length - 1].addChild(lbox);
-        };
-
-        //Movement step
-        addLayoutText(
-            panels,
-            locale("ui.label.movement"),
-            12,
-            false,
-            -1,
-            -1,
-            "Handel_Gothic_Regular.otf"
-        );
-        addHorizontalSubPanel(panels, 2);
-        addLayoutButton(
-            panels,
-            locale("ui.movement.finish_movement"),
-            "finishMove",
-            false,
-            10,
-            150,
-            -1
-        );
-        addGap(panels, 5, 0, 0, 0);
-        addLayoutButton(
-            panels,
-            "ui.roller.space_cannon_offense",
-            "spaceCannon",
-            false,
-            10,
-            150,
-            -1
-        );
-        panels.pop();
-
-        //Combat step
-        addLayoutText(
-            panels,
-            locale("ui.label.space_combat"),
-            12,
-            false,
-            -1,
-            -1,
-            "Handel_Gothic_Regular.otf"
-        );
-        addLayoutButton(
-            panels,
-            "ui.roller.anti_fighter_barrage",
-            "antiFighterBarrage",
-            false,
-            9,
-            200,
-            -1
-        );
-        addLayoutButton(
-            panels,
-            "ui.roller.announce_retreat",
-            "announceRetreat",
-            false,
-            9,
-            200,
-            -1
-        );
-        addLayoutButton(
-            panels,
-            "ui.roller.space_combat",
-            "spaceCombat",
-            false,
-            9,
-            200,
-            -1
-        );
-
-        //Invasion step
-        addLayoutText(
-            panels,
-            locale("ui.label.invasion"),
-            12,
-            false,
-            -1,
-            -1,
-            "Handel_Gothic_Regular.otf"
-        );
-        addHorizontalSubPanel(panels, 2);
-        system.planets.forEach((planet) => {
-            addPlanetSteps(panels, planet);
-            addGap(panels, 3, 0, 0, 0);
-        });
-        panels.pop();
-
-        //Production step
-        addLayoutText(
-            panels,
-            locale("ui.label.production"),
-            12,
-            false,
-            -1,
-            -1,
-            "Handel_Gothic_Regular.otf"
-        );
-        addLayoutButton(
-            panels,
-            locale("ui.label.production"),
-            "production",
-            false,
-            9,
-            200,
-            -1
-        );
-
-        //Extra
-        addVerticalSubPanel(panels, 0);
-        addLayoutButton(
-            panels,
-            "ui.roller.report_modifiers",
-            "reportModifiers",
-            false,
-            7,
-            100,
-            -1
-        );
-        panels.pop();
-
-        this.setChild(panels[panels.length - 1]);
         this._update();
     }
 }
