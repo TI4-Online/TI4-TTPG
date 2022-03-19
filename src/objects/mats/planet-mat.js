688a7c26322b9aeb349c8bc468c1399bdd2dfe0e
Darrell <darrell.anderson@gmail.com>
2022-03-19 15:05:41 -0700

    add popup button

--- src/objects/mats/planet-mat.js
+++ src/objects/mats/planet-mat.js
@@ -1,12 +1,19 @@
 const assert = require("../../wrapper/assert-wrapper");
 const locale = require("../../lib/locale");
+const { CardUtil } = require("../../lib/card/card-util");
 const {
+    Button,
     GameObject,
+    ImageButton,
+    UIElement,
+    Vector,
+    VerticalBox,
     globalEvents,
     refObject,
+    refPackageId,
     world,
 } = require("../../wrapper/api");
-const { CardUtil } = require("../../lib/card/card-util");
+const { Border } = require("@tabletop-playground/api");
 
 // Fetch cards
 class PlanetMat {
@@ -15,6 +22,25 @@ class PlanetMat {
 
         this._obj = gameObject;
         this._actionNameToPlanet = {};
+        this._popupUI = new UIElement();
+
+        const button = new ImageButton()
+            .setImage("global/ui/menu_button_hex.png", refPackageId)
+            .setImageSize(150, 150);
+        button.onClicked.add((button, player) => {
+            this.closePopupMenu();
+            this.createPopupMenu();
+        });
+
+        // Mallice needs to be flipped to see button, that's ok.
+        const ui = new UIElement();
+        ui.widget = button;
+        ui.position = new Vector(13.5, 0, 0.26);
+        ui.scale = 0.1;
+        this._obj.addUI(ui);
+
+        this._popupUI.position = new Vector(13.5, 0, 1);
+        this._popupUI.widget = new Border();
 
         globalEvents.TI4.onSystemActivated.add((systemTileObj, player) => {
             this.clearActionMenu();
@@ -49,6 +75,33 @@ class PlanetMat {
             this._actionNameToPlanet[actionName] = planet;
             this._obj.addCustomAction(actionName);
         }
+    }
+
+    closePopupMenu() {
+        this._obj.removeUIElement(this._popupUI);
+    }
+
+    createPopupMenu() {
+        console.log("PlanetMat.createPopupMenu()");
+        const panel = new VerticalBox();
+        for (const [actionName, planet] of Object.entries(
+            this._actionNameToPlanet
+        )) {
+            const button = new Button().setText(actionName);
+            button.onClicked.add((button, player) => {
+                this.closePopupMenu();
+                this.fetch(planet);
+            });
+            panel.addChild(button);
+        }
+        const button = new Button().setText(locale("ui.button.cancel"));
+        button.onClicked.add((button, player) => {
+            this.closePopupMenu();
+        });
+        panel.addChild(button);
+
+        this._popupUI.widget.setChild(panel);
+        this._obj.addUI(this._popupUI);
     }
 
     fetch(planet) {
