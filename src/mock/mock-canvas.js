0cf8bc79e5e4e7533c1041b9d1fa1c91e3523259
Darrell <darrell.anderson@gmail.com>
2022-03-22 12:22:19 -0700

    mocking

---
+++ src/mock/mock-canvas.js
@@ -0,0 +1,5 @@
+class Canvas {
+    addChild() {}
+}
+
+module.exports = Canvas;
---
+++ src/mock/mock-vertical-alignment.js
@@ -0,0 +1,8 @@
+class VerticalAlignment {
+    static Bottom = 3;
+    static Center = 2;
+    static Fill = 0;
+    static Top = 1;
+}
+
+module.exports = VerticalAlignment;
