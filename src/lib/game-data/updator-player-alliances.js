9bcc160cf7e119f7f56a3a33544a616511dc6d12
Darrell <darrell.anderson@gmail.com>
2022-03-30 14:47:48 -0700

    fill in placeholder game-data records

--- src/lib/game-data/updator-player-alliances.js
+++ src/lib/game-data/updator-player-alliances.js
@@ -1,4 +1,6 @@
 const assert = require("../../wrapper/assert-wrapper");
+const { CardUtil } = require("../card/card-util");
+const { ObjectNamespace } = require("../object-namespace");
 const { world } = require("../../wrapper/api");
 
 module.exports = (data) => {
@@ -7,5 +9,36 @@ module.exports = (data) => {
         playerData.alliances = [];
     });
 
-    // TODO XXX
+    const checkIsDiscardPile = false;
+    const allowFaceDown = false;
+    for (const obj of world.getAllObjects()) {
+        if (obj.getContainer()) {
+            continue;
+        }
+        if (!CardUtil.isLooseCard(obj, checkIsDiscardPile, allowFaceDown)) {
+            continue;
+        }
+        const nsid = ObjectNamespace.getNsid(obj);
+        if (
+            !nsid.startsWith("card.promissory") ||
+            !nsid.endsWith("/alliance")
+        ) {
+            continue;
+        }
+        const parsed = ObjectNamespace.parseNsid(nsid);
+        const parts = parsed.type.split(".");
+        const color = parts[2];
+
+        const pos = obj.getPosition();
+        const closestDesk = world.TI4.getClosestPlayerDesk(pos);
+        if (!closestDesk) {
+            continue;
+        }
+        const playerData = data.players[closestDesk.index];
+        assert(playerData);
+
+        if (!playerData.alliances.includes(color)) {
+            playerData.alliances.push(color);
+        }
+    }
 };
