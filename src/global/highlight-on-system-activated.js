4c4787b383be0f50bddb92d4a6e71c7d21d27f3f
Darrell <darrell.anderson@gmail.com>
2022-04-16 12:03:42 -0700

    highlight activated systems

--- src/global/highlight-on-system-activated.js
+++ src/global/highlight-on-system-activated.js
@@ -1,68 +1,58 @@
 const assert = require("../wrapper/assert-wrapper");
-const { Hex } = require("../lib/hex");
 const { ObjectNamespace } = require("../lib/object-namespace");
 const {
     Color,
+    ImageWidget,
+    Rotator,
+    UIElement,
     Vector,
-    ZoneShape,
     globalEvents,
+    refPackageId,
     world,
 } = require("../wrapper/api");
 
-const ZONE_H = 2;
-const ZONE_ALPHA = 0.2;
-const SAVED_DATA = "__highlight_on_system_activated__";
+const DISPLAY_SECONDS = 10;
 
-let _activatedSystemZone = undefined;
+let _img = undefined;
+let _obj = undefined;
+let _ui = undefined;
+let _removeUiHandle = undefined;
 
-function getZone(canAlloc) {
-    if (_activatedSystemZone && _activatedSystemZone.isValid()) {
-        return _activatedSystemZone;
+function applyHighlight(obj, color) {
+    // Keep the image around.
+    if (!_img) {
+        _img = new ImageWidget()
+            .setImageSize(110, 0)
+            .setImage("global/ui/hex_0_45_final.png", refPackageId);
     }
-    for (const zone of world.getAllZones()) {
-        if (zone.getSavedData() === SAVED_DATA) {
-            _activatedSystemZone = zone;
-            return _activatedSystemZone;
-        }
+    color = new Color(color.r, color.g, color.b, 1);
+    _img.setTintColor(color);
+
+    // Release and re-create the ui element.
+    if (_obj && _ui) {
+        _obj.removeUIElement(_ui);
     }
-    if (!canAlloc) {
-        return undefined;
+
+    _obj = obj;
+    _ui = new UIElement();
+    _ui.position = new Vector(0, 0, 0.21);
+    _ui.rotation = new Rotator(0, 0, 0);
+    _ui.widget = _img;
+    _obj.addUI(_ui);
+
+    // Schedule removal.  Do not try fancy things like fading, world.updateUI flashes ALL UI.
+    if (_removeUiHandle) {
+        clearTimeout(_removeUiHandle);
     }
-    _activatedSystemZone = world.createZone(
-        new Vector(0, 0, world.getTableHeight())
-    );
-    _activatedSystemZone.setAlwaysVisible(true);
-    _activatedSystemZone.setShape(ZoneShape.Hexagon);
-    _activatedSystemZone.setScale(
-        new Vector(Hex.HALF_SIZE * 2, Hex.HALF_SIZE * 2, ZONE_H)
-    );
-    _activatedSystemZone.setSavedData(SAVED_DATA);
-    return _activatedSystemZone;
+    _removeUiHandle = setTimeout(() => {
+        _obj.removeUIElement(_ui);
+        _removeUiHandle = undefined;
+    }, DISPLAY_SECONDS * 1000);
 }
 
-function applyHighlight(pos, color) {
-    pos.z = world.getTableHeight() + ZONE_H / 2 - 0.1;
-    const zone = getZone(true);
-    zone.setPosition(pos);
-    color = new Color(color.r, color.g, color.b, Math.max(0.1, ZONE_ALPHA));
-    zone.setColor(color);
-}
-
-// Register a listener to report (as well as test) system activation.
 globalEvents.TI4.onSystemActivated.add((obj, player) => {
     assert(ObjectNamespace.isSystemTile(obj));
-    let pos = obj.getPosition();
-    const hex = Hex.fromPosition(pos);
-    pos = Hex.toPosition(hex);
     const currentDesk = world.TI4.turns.getCurrentTurn();
-    const color = currentDesk ? currentDesk.color : new Color(1, 1, 0);
-    applyHighlight(pos, color);
-});
-
-// Destroy any existing zone.
-process.nextTick(() => {
-    const zone = getZone(false);
-    if (zone) {
-        zone.destroy();
-    }
+    const color = currentDesk ? currentDesk.plasticColor : new Color(1, 1, 0);
+    applyHighlight(obj, color);
 });
