c7ae9c3d78be15dc7c5ff783c6170cfa227753e9
Darrell <darrell.anderson@gmail.com>
2022-05-05 19:39:53 -0700

    wire fog enabled

--- src/game-ui/tab-map/tab-fog/tab-fog.js
+++ src/game-ui/tab-map/tab-fog/tab-fog.js
@@ -1,9 +1,12 @@
+const { world } = require("@tabletop-playground/api");
 const { TabFogOfWarUI } = require("./tab-fog-ui");
 
 class TabFogOfWar {
     constructor() {
         const onClickHandlers = {
-            toggleEnable: () => {},
+            toggleEnable: (checkBox, player, isChecked) => {
+                world.TI4.fogOfWar.setEnabled(isChecked);
+            },
         };
         this._ui = new TabFogOfWarUI(onClickHandlers);
     }
