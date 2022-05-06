3fe258249fc47125740eeffb8f159dbc4f6cdba5
Darrell <darrell.anderson@gmail.com>
2022-05-06 12:09:26 -0700

    collect and merge line segments

--- src/lib/borders/borders.js
+++ src/lib/borders/borders.js
@@ -7,11 +7,194 @@
 const assert = require("../../wrapper/assert-wrapper");
 const { Hex } = require("../../lib/hex");
 const { ObjectNamespace } = require("../object-namespace");
+const PositionToPlanet = require("../system/position-to-planet");
 const { UnitAttrsSet } = require("../unit/unit-attrs-set");
 const { UnitPlastic } = require("../unit/unit-plastic");
-const { world } = require("../../wrapper/api");
+const { GameObject, world } = require("../../wrapper/api");
+
+const AREA = {
+    PLANET: 1,
+    SPACE: 2,
+};
 
 class Borders {
+    /**
+     * Find control tokens and units on system hexes.
+     *
+     * @returns {Array} list of control entries
+     */
+    static getAllControlEntries() {
+        const hexToSystemObject = [];
+        for (const systemObject of world.TI4.getAllSystemTileObjects()) {
+            if (systemObject.getContainer()) {
+                continue;
+            }
+            const pos = systemObject.getPosition();
+            const hex = Hex.fromPosition(pos);
+            hexToSystemObject[hex] = systemObject; // pick one if stacked
+        }
+
+        const controlEntries = [];
+        const maybeAddControlEntry = (obj, hex, areaType) => {
+            assert(obj instanceof GameObject);
+            assert(typeof hex === "string");
+            assert(typeof areaType === "number");
+
+            const systemObj = hexToSystemObject[hex];
+            if (!systemObj) {
+                return;
+            }
+            let planet = undefined;
+            if (areaType === AREA.PLANET) {
+                const pos = obj.getPosition();
+                planet = PositionToPlanet.getExactPlanet(pos, systemObj);
+                if (!planet) {
+                    return;
+                }
+            }
+
+            const playerSlot = obj.getOwningPlayerSlot();
+            if (playerSlot < 0) {
+                return;
+            }
+
+            controlEntries.push({
+                obj,
+                hex,
+                areaType,
+                planet,
+                playerSlot,
+            });
+        };
+
+        const unitAttrsSet = new UnitAttrsSet();
+        for (const unitPlastic of UnitPlastic.getAll()) {
+            if (unitPlastic.owningPlayerSlot < 0) {
+                continue;
+            }
+            const unitAttrs = unitAttrsSet.get(unitPlastic.unit);
+            if (!unitAttrs) {
+                throw new Error(`unknown unit "${unitPlastic.unit}"`);
+            }
+            if (unitAttrs.raw.structure || unitAttrs.raw.ground) {
+                maybeAddControlEntry(
+                    unitPlastic.gameObject,
+                    unitPlastic.hex,
+                    AREA.PLANET
+                );
+            } else if (unitAttrs.raw.ship) {
+                maybeAddControlEntry(
+                    unitPlastic.gameObject,
+                    unitPlastic.hex,
+                    AREA.SPACE
+                );
+            } else {
+                throw new Error(
+                    `unit "${unitPlastic.unit}" is neither planet nor space?`
+                );
+            }
+        }
+
+        for (const obj of world.getAllObjects()) {
+            if (obj.getContainer()) {
+                continue;
+            }
+            if (!ObjectNamespace.isControlToken(obj)) {
+                continue;
+            }
+            const pos = obj.getPosition();
+            const hex = Hex.fromPosition(pos);
+            maybeAddControlEntry(obj, hex, AREA.PLANET);
+        }
+
+        return controlEntries;
+    }
+
+    static getHexToControlSummary(controlEntries) {
+        assert(Array.isArray(controlEntries));
+
+        const hexToControlSummary = {};
+        for (const controlEntry of controlEntries) {
+            const hex = controlEntry.hex;
+            assert(typeof hex === "string");
+            const playerSlot = controlEntry.playerSlot;
+            assert(playerSlot >= 0);
+
+            let summary = hexToControlSummary[hex];
+            if (!summary) {
+                summary = {};
+                hexToControlSummary[hex] = summary;
+            }
+            const area = controlEntry.planet
+                ? controlEntry.planet.planetIndex
+                : "space";
+            const current = summary[area];
+            if (current === undefined) {
+                summary[area] = playerSlot;
+            } else if (current !== playerSlot) {
+                // Conflicting control for area.
+                summary[area] = -1;
+            }
+        }
+
+        return hexToControlSummary;
+    }
+
+    static getSpaceLineSegments(hexToControlSummary) {
+        const segments = [];
+
+        // Clockwise winding for inset
+        for (const [hex, summary] of Object.entries(hexToControlSummary)) {
+            const playerSlot = summary.space;
+            if (playerSlot === undefined || playerSlot < 0) {
+                continue;
+            }
+            const corners = Hex.corners(hex); // top right, then counterclockwise
+            const neighbors = Hex.neighbors(hex); // top, then counterclockwise
+            for (let i = 0; i < neighbors.length; i++) {
+                const neighbor = neighbors[i];
+                const neighborSummary = hexToControlSummary[neighbor];
+                if (
+                    neighborSummary &&
+                    neighborSummary.space === summary.space
+                ) {
+                    continue;
+                }
+                const a = corners[i];
+                const b = corners[(i + 1) % corners.length];
+                segments.push({ line: [a, b], playerSlot });
+            }
+        }
+        return segments;
+    }
+
+    static linkLineSegments(segments) {
+        const result = [];
+
+        for (const segment of segments) {
+            const [a, b] = segment.line;
+            let found = false;
+            for (const candidate of result) {
+                if (candidate.playerSlot != segment.playerSlot) {
+                    continue;
+                }
+                const line = candidate.line;
+                const last = line[line.length - 1];
+                const d = last.subtract(a).magnitudeSquared();
+                if (d > 0.1) {
+                    continue;
+                }
+                found = true;
+                line.push(b);
+                break;
+            }
+            if (!found) {
+                result.push(segment);
+            }
+        }
+        return result;
+    }
+
     constructor() {
         this._enabled = false;
         this._lines = undefined;
@@ -27,48 +210,6 @@ class Borders {
             this._lines = undefined;
         }
     }
-
-    update() {
-        const spaceObjs = [];
-        const planetObjs = [];
-
-        const unitAttrsSet = new UnitAttrsSet();
-        for (const unitPlastic of UnitPlastic.getAll()) {
-            if (unitPlastic.owningPlayerSlot < 0) {
-                continue;
-            }
-            const unitAttrs = unitAttrsSet.get(unitPlastic.unit);
-            if (!unitAttrs) {
-                throw new Error(`unknown unit "${unitPlastic.unit}"`);
-            }
-            if (unitAttrs.raw.structure || unitAttrs.raw.ground) {
-                planetObjs.push(unitPlastic.gameObject);
-            } else if (unitAttrs.raw.ship) {
-                spaceObjs.push(unitPlastic.gameObject);
-            } else {
-                throw new Error(
-                    `unit "${unitPlastic.unit}" is neither planet nor space?`
-                );
-            }
-        }
-
-        for (const obj of world.getAllObjects()) {
-            if (obj.getContainer()) {
-                continue;
-            }
-            if (!ObjectNamespace.isControlToken(obj)) {
-                continue;
-            }
-            planetObjs.push(obj);
-        }
-
-        const hexToSystemObject = [];
-        for (const systemObject of world.TI4.getAllSystemTileObjects()) {
-            const pos = systemObject.getPosition();
-            const hex = Hex.fromPosition(pos);
-            hexToSystemObject[hex] = systemObject;
-        }
-    }
 }
 
-module.exports = { Borders };
+module.exports = { Borders, AREA };
---
+++ src/lib/borders/borders.test.js
@@ -0,0 +1,145 @@
+require("../../global"); // setup world.TI4
+const assert = require("assert");
+const { Borders, AREA } = require("./borders");
+const { MockGameObject, MockVector, world } = require("../../wrapper/api");
+
+it("getAllControlEntries", () => {
+    const systemObj = new MockGameObject({
+        templateMetadata: "tile.system:base/18",
+        position: new MockVector(0, 0, 0),
+    });
+    const controlToken = new MockGameObject({
+        templateMetadata: "token.control:base/arborec",
+        position: new MockVector(0, 0, 0),
+        owningPlayerSlot: 1,
+    });
+    const pds = new MockGameObject({
+        templateMetadata: "unit:base/pds",
+        position: new MockVector(0, 0, 0),
+        owningPlayerSlot: 2,
+    });
+    const dreadnought = new MockGameObject({
+        templateMetadata: "unit:base/dreadnought",
+        position: new MockVector(0, 0, 0),
+        owningPlayerSlot: 3,
+    });
+
+    world.__clear();
+    world.__addObject(systemObj);
+    world.__addObject(controlToken);
+    world.__addObject(pds);
+    world.__addObject(dreadnought);
+
+    const controlEntries = Borders.getAllControlEntries();
+    world.__clear;
+
+    assert.equal(controlEntries.length, 3);
+    assert.equal(controlEntries[0].obj, pds);
+    assert.equal(controlEntries[0].hex, "<0,0,0>");
+    assert.equal(controlEntries[0].playerSlot, 2);
+    assert.equal(controlEntries[0].areaType, AREA.PLANET);
+    assert(controlEntries[0].planet);
+
+    assert.equal(controlEntries[1].obj, dreadnought);
+    assert.equal(controlEntries[1].hex, "<0,0,0>");
+    assert.equal(controlEntries[1].playerSlot, 3);
+    assert.equal(controlEntries[1].areaType, AREA.SPACE);
+    assert(!controlEntries[1].planet);
+
+    assert.equal(controlEntries[2].obj, controlToken); // units found first
+    assert.equal(controlEntries[2].hex, "<0,0,0>");
+    assert.equal(controlEntries[2].playerSlot, 1);
+    assert.equal(controlEntries[2].areaType, AREA.PLANET);
+    assert(controlEntries[2].planet);
+});
+
+it("getHexToControlSummary", () => {
+    const systemObj = new MockGameObject({
+        templateMetadata: "tile.system:base/18",
+        position: new MockVector(0, 0, 0),
+    });
+    const controlToken = new MockGameObject({
+        templateMetadata: "token.control:base/arborec",
+        position: new MockVector(0, 0, 0),
+        owningPlayerSlot: 1,
+    });
+    const pds = new MockGameObject({
+        templateMetadata: "unit:base/pds",
+        position: new MockVector(0, 0, 0),
+        owningPlayerSlot: 2,
+    });
+    const dreadnought = new MockGameObject({
+        templateMetadata: "unit:base/dreadnought",
+        position: new MockVector(0, 0, 0),
+        owningPlayerSlot: 3,
+    });
+
+    world.__clear();
+    world.__addObject(systemObj);
+    world.__addObject(controlToken);
+    world.__addObject(pds);
+    world.__addObject(dreadnought);
+
+    const controlEntries = Borders.getAllControlEntries();
+    const hexToControlSummary = Borders.getHexToControlSummary(controlEntries);
+    world.__clear;
+
+    const summary = hexToControlSummary["<0,0,0>"];
+    assert(summary);
+
+    assert.equal(summary["space"], 3);
+    assert.equal(summary[0], -1);
+});
+
+it("getSpaceLineSegments", () => {
+    const systemObj = new MockGameObject({
+        templateMetadata: "tile.system:base/18",
+        position: new MockVector(0, 0, 0),
+    });
+    const controlToken = new MockGameObject({
+        templateMetadata: "unit:base/dreadnought",
+        position: new MockVector(0, 0, 0),
+        owningPlayerSlot: 1,
+    });
+
+    world.__clear();
+    world.__addObject(systemObj);
+    world.__addObject(controlToken);
+
+    const controlEntries = Borders.getAllControlEntries();
+    const hexToControlSummary = Borders.getHexToControlSummary(controlEntries);
+    const lineSegments = Borders.getSpaceLineSegments(hexToControlSummary);
+    world.__clear;
+
+    assert.equal(lineSegments.length, 6);
+    assert.equal(lineSegments[0].playerSlot, 1);
+});
+
+it("linkLineSegments", () => {
+    const systemObj = new MockGameObject({
+        templateMetadata: "tile.system:base/18",
+        position: new MockVector(0, 0, 0),
+    });
+    const controlToken = new MockGameObject({
+        templateMetadata: "unit:base/dreadnought",
+        position: new MockVector(0, 0, 0),
+        owningPlayerSlot: 1,
+    });
+
+    world.__clear();
+    world.__addObject(systemObj);
+    world.__addObject(controlToken);
+
+    const controlEntries = Borders.getAllControlEntries();
+    const hexToControlSummary = Borders.getHexToControlSummary(controlEntries);
+    const lineSegments = Borders.getSpaceLineSegments(hexToControlSummary);
+    const linkedSegments = Borders.linkLineSegments(lineSegments);
+    world.__clear;
+
+    assert.equal(linkedSegments.length, 1);
+
+    const line = linkedSegments[0].line;
+    assert.equal(line.length, 7); // closed loop
+    const d = line[0].subtract(line[6]).magnitudeSquared();
+    assert(d < 0.1);
+});
