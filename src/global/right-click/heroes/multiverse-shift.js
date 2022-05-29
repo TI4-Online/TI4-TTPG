ed36a1893df19260b5d62a7b92d9b93ee92c33ca
11qats <11qats@gmail.com>
2022-05-29 19:53:12 -0400

    add automation for empyrean hero

---
+++ src/global/right-click/heroes/multiverse-shift.js
@@ -0,0 +1,184 @@
+const assert = require("../../../wrapper/assert-wrapper");
+const locale = require("../../../lib/locale");
+const { Broadcast } = require("../../../lib/broadcast");
+const { Hex } = require("../../../lib/hex");
+const { ObjectNamespace } = require("../../../lib/object-namespace");
+const { UnitPlastic } = require("../../../lib/unit/unit-plastic");
+const { SystemHighlight } = require("../../highlight-on-system-activated");
+const { Spawn } = require("../../../setup/spawn/spawn");
+const {
+    Card,
+    world,
+    globalEvents,
+    Rotator,
+    GameObject,
+} = require("../../../wrapper/api");
+
+const CARD_NAME = "conservator_procyon";
+const PURGE_CONTAINER_NAME = "bag.purge";
+const ACTION_NAME = "*" + locale("ui.menu.multiverse_shift");
+const FRONTIER_TOKEN_NSID = "token:pok/frontier";
+const SHIPS = [
+    "carrier",
+    "cruiser",
+    "destroyer",
+    "dreadnought",
+    "flagship",
+    "war_sun",
+];
+
+function multiverseShift(card) {
+    assert(card instanceof Card);
+    const cardName = ObjectNamespace.parseCard(card).name;
+    assert(cardName === CARD_NAME);
+
+    let cardOwnerSlot = -1;
+    let ownerColor;
+    for (const desk of world.TI4.getAllPlayerDesks()) {
+        const faction = world.TI4.getFactionByPlayerSlot(desk.playerSlot);
+        if (faction && faction.raw.leaders.heroes.includes(CARD_NAME)) {
+            cardOwnerSlot = desk.playerSlot;
+            ownerColor = desk.plasticColor;
+            break;
+        }
+    }
+
+    if (cardOwnerSlot === -1) {
+        Broadcast.broadcastAll(
+            locale("ui.message.multiverse_shift_has_no_owner")
+        );
+        return;
+    }
+
+    const playerName = world.TI4.getNameByPlayerSlot(cardOwnerSlot);
+    Broadcast.broadcastAll(
+        locale("ui.message.multiverse_shift", { playerName })
+    );
+
+    // determine which hexes already have frontier tokens in them
+    const frontierTokenHexes = new Set();
+    const hexToExistingFrontierTokens = {};
+    for (const obj of world.getAllObjects()) {
+        if (obj.getContainer()) {
+            continue;
+        }
+        const nsid = ObjectNamespace.getNsid(obj);
+        if (nsid !== FRONTIER_TOKEN_NSID) {
+            continue;
+        }
+        const pos = obj.getPosition();
+        const hex = Hex.fromPosition(pos);
+        frontierTokenHexes.add(hex);
+        hexToExistingFrontierTokens[hex] = obj;
+    }
+
+    // add frontier tokens to all empty systems that don't already have one
+    for (const obj of world.TI4.getAllSystemTileObjects()) {
+        const system = world.TI4.getSystemBySystemTileObject(obj);
+        if (system.planets.length > 0 || system.hyperlane) {
+            continue;
+        }
+
+        const hex = Hex.fromPosition(obj.getPosition());
+
+        let frontierTokenObj = undefined;
+        if (!frontierTokenHexes.has(hex)) {
+            // drop new frontier token here
+            const dy = -2.5 * Hex.SCALE;
+            const tokenPos = obj.getPosition().add([0, dy, 1]);
+            frontierTokenObj = Spawn.spawn(
+                FRONTIER_TOKEN_NSID,
+                tokenPos,
+                new Rotator(0, 0, 0)
+            );
+        } else {
+            frontierTokenObj = hexToExistingFrontierTokens[hex];
+        }
+
+        const ownersShipsInHex = UnitPlastic.getAll().filter((plastic) => {
+            return plastic.hex === hex && SHIPS.includes(plastic.unit);
+        });
+
+        if (ownersShipsInHex.length > 0) {
+            // card owner has ships here, highlight the system to make it easier to find and explore
+            const systemHighlight = new SystemHighlight(obj, ownerColor, true);
+
+            // remove the highlighting once the token is destroyed from the explore
+            frontierTokenObj.onDestroyed.add(() => {
+                systemHighlight.detachUI();
+            });
+        }
+    }
+
+    // purge the card
+    let purgeContainer = null;
+    for (const obj of world.getAllObjects()) {
+        if (obj.getName() === locale(PURGE_CONTAINER_NAME)) {
+            purgeContainer = obj;
+            break;
+        }
+    }
+
+    if (purgeContainer) {
+        Broadcast.broadcastAll(
+            locale("ui.message.purge", { objectName: locale(cardName) })
+        );
+        purgeContainer.addObjects([card], true);
+    }
+}
+
+function maybeMultiverseShift(card, _player, selectedActionName) {
+    if (selectedActionName === ACTION_NAME) {
+        multiverseShift(card);
+    }
+}
+
+function addRightClickOption(card) {
+    assert(card instanceof Card);
+    removeRightClickOption(card);
+    card.addCustomAction(ACTION_NAME);
+    card.onCustomAction.add(maybeMultiverseShift);
+}
+
+function removeRightClickOption(card) {
+    card.removeCustomAction(ACTION_NAME);
+    card.onCustomAction.remove(maybeMultiverseShift);
+}
+
+function isConservatorProcyon(obj) {
+    assert(obj instanceof GameObject);
+
+    if (!(obj instanceof Card)) {
+        return false;
+    }
+
+    if (!ObjectNamespace.isCard(obj)) {
+        return false;
+    }
+
+    const parsedCard = ObjectNamespace.parseCard(obj);
+    if (parsedCard.name === CARD_NAME) {
+        return true;
+    }
+}
+
+globalEvents.TI4.onSingletonCardCreated.add((obj) => {
+    if (isConservatorProcyon(obj)) {
+        addRightClickOption(obj);
+        obj.__hasRightClickMultiverseShift = true;
+    }
+});
+
+globalEvents.TI4.onSingletonCardMadeDeck.add((obj) => {
+    if (obj.__hasRightClickMultiverseShift) {
+        removeRightClickOption(obj);
+        delete obj.__hasRightClickMultiverseShift;
+    }
+});
+
+for (const obj of world.getAllObjects()) {
+    if (isConservatorProcyon(obj)) {
+        addRightClickOption(obj);
+        obj.__hasRightClickMultiverseShift = true;
+    }
+}
