34a5c73356cf4ee5c3f89e22fb264c2ea795c823
Darrell <darrell.anderson@gmail.com>
2022-06-29 16:08:46 -0700

    turn change sound now only plays for the current player

--- src/global/on-turn-changed-sound.js
+++ src/global/on-turn-changed-sound.js
@@ -1,25 +1,35 @@
 const assert = require("../wrapper/assert-wrapper");
-const { globalEvents, refPackageId, world } = require("../wrapper/api");
+const {
+    PlayerPermission,
+    globalEvents,
+    refPackageId,
+    world,
+} = require("../wrapper/api");
 
 // Relative to assets/Sounds/
 const SOUND_ASSET = "beep_ramp_up.wav";
 
 let _sound = undefined;
 
-function playTurnChangedSound() {
+function playTurnChangedSound(playerSlot) {
+    assert(typeof playerSlot === "number");
+
     if (!_sound) {
         _sound = world.importSound(SOUND_ASSET, refPackageId);
         assert(_sound);
     }
 
+    const playerPermission = new PlayerPermission();
+    playerPermission.setPlayerSlots([playerSlot]);
+
     const startTime = 0;
     const volume = 0.5; // [0:2] range
     const loop = false;
-    _sound.play(startTime, volume, loop);
+    _sound.play(startTime, volume, loop, playerPermission);
 }
 
 globalEvents.TI4.onTurnChanged.add(
     (currentDesk, previousDesk, clickingPlayer) => {
-        playTurnChangedSound();
+        playTurnChangedSound(currentDesk.playerSlot);
     }
 );
