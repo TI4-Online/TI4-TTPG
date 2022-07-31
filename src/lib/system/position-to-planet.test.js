9f6ce8a5d019682b6c8fdd70479eea040da42c16
Darrell <darrell.anderson@gmail.com>
2022-07-30 17:08:54 -0700

    test position to planet for all planets.  remove ignoring destroyed planets, stellar converter was going nuts b/c it would not find where it was.  maybe revisit this later but keep it simple(r) for now.

--- src/lib/system/position-to-planet.test.js
+++ src/lib/system/position-to-planet.test.js
@@ -1,6 +1,7 @@
 require("../../global"); // create globalEvents.TI4
 const assert = require("assert");
 const PositionToPlanet = require("./position-to-planet");
+const SYSTEM_ATTRS = require("./system.data");
 const { MockGameObject, MockVector, world } = require("../../wrapper/api");
 
 it("getClosestPlanet", () => {
@@ -15,4 +16,32 @@ it("getClosestPlanet", () => {
     const planet = PositionToPlanet.getClosestPlanet(pos, systemObject, false);
     world.__clear();
     assert.equal(planet.localeName, "planet.mecatol_rex");
+});
+
+it("check all planets", () => {
+    for (const raw of SYSTEM_ATTRS) {
+        const tile = raw.tile;
+        const system = world.TI4.getSystemByTileNumber(tile);
+        assert(system.tile === tile);
+        world.__clear();
+        const systemObject = new MockGameObject({
+            templateMetadata: `tile.system:base/${tile}`,
+            position: new MockVector(0, 0, 0),
+        });
+        for (const planet of system.planets) {
+            const pos = systemObject.localPositionToWorld(planet.position);
+            const planet2 = PositionToPlanet.getClosestPlanet(
+                pos,
+                systemObject,
+                false
+            );
+            if (planet !== planet2) {
+                throw new Error(
+                    `mismatch ${tile} "${planet.getNameStr()}" vs "${planet2.getNameStr()}"`
+                );
+            }
+            assert(planet === planet2);
+        }
+    }
+    world.__clear();
 });
