dc1744d92d5a3b70d5a3e824409506d646c734cd
Darrell <darrell.anderson@gmail.com>
2022-10-11 15:53:59 -0700

    scored secrets

---
+++ src/game-ui/tab-stats/tab-secrets/tab-secrets-ui.js
@@ -0,0 +1,45 @@
+const CONFIG = require("../../game-ui-config");
+const {
+    HorizontalBox,
+    Text,
+    TextJustification,
+} = require("../../../wrapper/api");
+
+class TabSecretsUI extends HorizontalBox {
+    constructor() {
+        super();
+
+        this.setChildDistance(CONFIG.spacing);
+
+        const fontSize = CONFIG.fontSize * 0.6;
+        this._columns = new Array(3).fill(0).map(() => {
+            const column = new Text()
+                .setFontSize(fontSize)
+                .setJustification(TextJustification.Center);
+            this.addChild(column, 1);
+            return column;
+        });
+    }
+
+    update(allSecretNames, scoredSecretNames) {
+        this._columns.forEach((column) => column.setText(""));
+
+        const scoredSet = new Set(scoredSecretNames);
+
+        const count = Math.ceil(allSecretNames.length / this._columns.length);
+        this._columns.forEach((column, index) => {
+            const names = allSecretNames.splice(0, count);
+            const value = names
+                .map((name) => {
+                    if (scoredSet.has(name)) {
+                        return `~~${name}~~`;
+                    }
+                    return name;
+                })
+                .join("\n");
+            column.setText(value);
+        });
+    }
+}
+
+module.exports = { TabSecretsUI };
