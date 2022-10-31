2c15be23bd2392895bf8019f7897aee1c7e0a358
Darrell <darrell.anderson@gmail.com>
2022-10-31 10:04:16 -0700

    toggle button to show/hide turn order UI

--- src/global/screen-ui/turn-order.js
+++ src/global/screen-ui/turn-order.js
@@ -2,19 +2,31 @@ const { TurnOrderPanel } = require("../../lib/ui/turn-order/turn-order-panel");
 const {
     Border,
     HorizontalAlignment,
+    ImageButton,
     LayoutBox,
+    PlayerPermission,
     ScreenUIElement,
+    VerticalAlignment,
     globalEvents,
+    refPackageId,
     world,
 } = require("../../wrapper/api");
 
 const WIDTH = 220;
 const ENTRY_HEIGHT = 58;
-const PAD = 10;
+const PAD = 14;
+
+const BUTTON_SIZE = 10;
 
 class TurnOrderScreenUI {
     constructor() {
+        this._playerSlots = Array.from(Array(20).keys());
+        this._playerPermission = new PlayerPermission().setPlayerSlots(
+            this._playerSlots
+        );
+
         this._fancyUIs = [this.createFancyUI()];
+        this.createToggleUI();
 
         globalEvents.TI4.onPlayerCountChanged.add((playerCount) => {
             this.resetHeight();
@@ -22,6 +34,79 @@ class TurnOrderScreenUI {
         this.resetHeight();
     }
 
+    resetHeight() {
+        for (const ui of this._fancyUIs) {
+            ui.height = ENTRY_HEIGHT * world.TI4.config.playerCount + PAD;
+            world.updateScreenUI(ui);
+        }
+    }
+
+    toggle(player) {
+        console.log(`TurnOrderScreenUI.toggle "${player.getName()}"`);
+
+        const playerSlot = player.getSlot();
+        const index = this._playerSlots.indexOf(playerSlot);
+        if (index >= 0) {
+            this._playerSlots.splice(index, 1);
+        } else {
+            this._playerSlots.push(playerSlot);
+        }
+
+        this._playerPermission.setPlayerSlots(this._playerSlots);
+
+        for (const ui of this._fancyUIs) {
+            world.updateScreenUI(ui);
+        }
+    }
+
+    createToggleUI() {
+        const c = 0.3;
+        const button = new ImageButton()
+            .setImage("global/ui/white16x16.png", refPackageId)
+            .setImageSize(BUTTON_SIZE, BUTTON_SIZE)
+            .setTintColor([c, c, c, 1]);
+        button.onClicked.add((button, player) => {
+            this.toggle(player);
+        });
+
+        // Screen UI can be placed and sized with relative values, but that
+        // means UI will vary with screen size.  Since we cannot (yet) center
+        // a fixed-size element, place a fixed-side box inside another.
+        // This is definitely a hack.
+        // This goes away when screen UI can be fixed size.
+        const imageButtonBorder = 2;
+        const right =
+            Math.round(PAD + WIDTH / 2 - BUTTON_SIZE / 2) - imageButtonBorder;
+        const top = Math.round(PAD - BUTTON_SIZE / 2) - imageButtonBorder;
+        const buttonBox = new LayoutBox()
+            .setPadding(0, right, top, 0)
+            .setHorizontalAlignment(HorizontalAlignment.Right)
+            .setVerticalAlignment(VerticalAlignment.Top)
+            .setChild(button);
+
+        const ui = new ScreenUIElement();
+        ui.relativeWidth = true;
+        ui.width = 0.2;
+        ui.relativeHeight = false;
+        ui.height = BUTTON_SIZE + PAD;
+        ui.relativePositionX = true;
+        ui.positionX = 1 - ui.width;
+
+        ui.relativePositionY = false;
+        ui.positionY = 0;
+
+        ui.widget = buttonBox;
+
+        world.addScreenUI(ui);
+
+        return ui;
+    }
+
+    /**
+     * Turn order list, positioned in upper right slightly offset from screen edge.
+     *
+     * @returns {ScreenUIElement}
+     */
     createFancyUI() {
         // Fancy hard codes font sizes and fit lengths.
         const turnOrderPanel = new TurnOrderPanel().setUseFancyWidgets(true);
@@ -55,17 +140,11 @@ class TurnOrderScreenUI {
         ui.positionY = 0;
 
         ui.widget = outer;
+        ui.players = this._playerPermission;
 
         world.addScreenUI(ui);
 
         return ui;
-    }
-
-    resetHeight() {
-        for (const ui of this._fancyUIs) {
-            ui.height = ENTRY_HEIGHT * world.TI4.config.playerCount + PAD;
-            world.updateScreenUI(ui);
-        }
     }
 }
 
