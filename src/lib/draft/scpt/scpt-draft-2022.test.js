ed52583cc529781328a9526a39c3701cf48193b7
Darrell <darrell.anderson@gmail.com>
2022-11-13 17:06:28 -0800

    move test

--- src/lib/draft/scpt/scpt-draft-2022.test.js
+++ src/lib/draft/scpt/scpt-draft-2022.test.js
@@ -1,11 +1,11 @@
 require("../../../global"); // declare world.TI4
 const assert = require("assert");
 const { FactionAliases } = require("../../faction/faction-aliases");
-const { SCPT_DRAFTS } = require("./scpt-draft.data");
+const { SCPT_DRAFTS_2022 } = require("./scpt-draft-2022.data");
 const { world } = require("../../../wrapper/api");
 
 it("faction names", () => {
-    for (const scptDraft of SCPT_DRAFTS) {
+    for (const scptDraft of SCPT_DRAFTS_2022) {
         for (const factionSet of scptDraft.factionSets) {
             const factionNames = factionSet.split("|");
             for (const factionName of factionNames) {
