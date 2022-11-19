5d1c3889b2893d82c69ea3f4acb0382fcfd917f2
Darrell <darrell.anderson@gmail.com>
2022-11-19 14:13:27 -0800

    rework roller UI to edit widgets in place instead of reset/re-create UI

--- src/objects/roller/auto-roller-ui.js
+++ src/objects/roller/auto-roller-ui.js
@@ -4,6 +4,7 @@ const { AutoRollerArena } = require("./auto-roller-arena");
 const { ColorUtil } = require("../../lib/color/color-util");
 const { Hex } = require("../../lib/hex");
 const { System, Planet } = require("../../lib/system/system");
+const { ThrottleClickHandler } = require("../../lib/ui/throttle-click-handler");
 const CONFIG = require("../../game-ui/game-ui-config");
 const {
     Border,
@@ -95,68 +96,16 @@ class AutoRollerUI extends HorizontalBox {
         super();
         this._onButton = onButton;
 
-        this._stepsPanel = new VerticalBox()
-            .setChildDistance(VERTICAL_DISTANCE)
-            .setVerticalAlignment(VerticalAlignment.Fill);
-        this._invasionPanel = new VerticalBox().setChildDistance(
-            VERTICAL_DISTANCE
-        );
-        const arenaPanel = new VerticalBox().setChildDistance(
-            VERTICAL_DISTANCE
-        );
-
-        // Divide arena panel up.
-        this._arenaBox = new LayoutBox()
-            .setHorizontalAlignment(HorizontalAlignment.Center)
-            .setVerticalAlignment(VerticalAlignment.Center);
-        const arenaBoxBorder = new Border()
-            .setColor(ColorUtil.colorFromHex("#101010"))
-            .setChild(this._arenaBox);
-        this._arenaLowerLeft = new VerticalBox().setChildDistance(
-            VERTICAL_DISTANCE
-        );
-        const arenaLowerRight = new VerticalBox();
-        const arenaLower = new HorizontalBox()
-            .setChildDistance(CONFIG.spacing)
-            .addChild(this._arenaLowerLeft, 1)
-            .addChild(new Border().setColor(CONFIG.spacerColor))
-            .addChild(arenaLowerRight, 1);
-        arenaPanel.addChild(arenaBoxBorder, 9);
-        arenaPanel.addChild(arenaLower, 3);
-
-        arenaLowerRight
-            .addChild(
-                new Text()
-                    .setFontSize(HELP_FONT_SIZE)
-                    .setText(locale("ui.help.numpad"))
-            )
-            .addChild(
-                new Text()
-                    .setFontSize(HELP_FONT_SIZE)
-                    .setText(locale("ui.help.numpad.4"))
-            )
-            .addChild(
-                new Text()
-                    .setFontSize(HELP_FONT_SIZE)
-                    .setText(locale("ui.help.numpad.5"))
-            )
-            .addChild(
-                new Text()
-                    .setFontSize(HELP_FONT_SIZE)
-                    .setText(locale("ui.help.numpad.9"))
-            )
-            .addChild(
-                new Text()
-                    .setFontSize(HELP_FONT_SIZE)
-                    .setText(locale("ui.help.numpad.0"))
-            );
+        this._createAndLayoutStepsPanel();
+        this._createAndLayoutInvasionPanel();
+        this._createAndLayoutArenaPanel();
 
         this.setChildDistance(CONFIG.spacing)
             .addChild(this._stepsPanel, 1)
             .addChild(new Border().setColor(CONFIG.spacerColor))
             .addChild(this._invasionPanel, 1)
             .addChild(new Border().setColor(CONFIG.spacerColor))
-            .addChild(arenaPanel, 2);
+            .addChild(this._arenaPanel, 2);
 
         this.resetAwaitingSystemActivation();
 
@@ -194,9 +143,11 @@ class AutoRollerUI extends HorizontalBox {
         const button = new Button()
             .setFontSize(BUTTON_FONT_SIZE)
             .setText(locale(localeText));
-        button.onClicked.add((button, player) => {
-            this._onButton(combatType, planet, player);
-        });
+        button.onClicked.add(
+            ThrottleClickHandler.wrap((button, player) => {
+                this._onButton(combatType, planet, player);
+            })
+        );
         return button;
     }
 
@@ -204,10 +155,11 @@ class AutoRollerUI extends HorizontalBox {
         return new LayoutBox().setOverrideHeight(VERTICAL_DISTANCE);
     }
 
-    _fillStepsPanel(system) {
-        assert(system instanceof System);
-
-        this._stepsPanel.removeAllChildren();
+    _createAndLayoutStepsPanel() {
+        assert(!this._stepsPanel);
+        this._stepsPanel = new VerticalBox()
+            .setChildDistance(VERTICAL_DISTANCE)
+            .setVerticalAlignment(VerticalAlignment.Fill);
 
         let localeText;
         let combatType;
@@ -220,13 +172,16 @@ class AutoRollerUI extends HorizontalBox {
 
         localeText = "ui.movement.finish_movement";
         combatType = COMBAT_TYPE.FINISH_MOVE;
-        widget = this._createButton(localeText, combatType);
-        this._stepsPanel.addChild(widget, BUTTON_WEIGHT);
+        this._stepsFinishMove = this._createButton(localeText, combatType);
+        this._stepsPanel.addChild(this._stepsFinishMove, BUTTON_WEIGHT);
 
         localeText = "ui.roller.space_cannon_offense";
         combatType = COMBAT_TYPE.SPACE_CANNON;
-        widget = this._createButton(localeText, combatType);
-        this._stepsPanel.addChild(widget, BUTTON_WEIGHT);
+        this._stepsSpaceCannonOffense = this._createButton(
+            localeText,
+            combatType
+        );
+        this._stepsPanel.addChild(this._stepsSpaceCannonOffense, BUTTON_WEIGHT);
 
         // GAP
         widget = this._createGap();
@@ -237,28 +192,28 @@ class AutoRollerUI extends HorizontalBox {
         widget = this._createLabel(localeText);
         this._stepsPanel.addChild(widget, LABEL_WEIGHT);
 
-        const ambushAvailable = AutoRollerUI._ambush(system);
-        if (ambushAvailable) {
-            localeText = "ui.roller.ambush";
-            combatType = COMBAT_TYPE.AMBUSH;
-            widget = this._createButton(localeText, combatType);
-            this._stepsPanel.addChild(widget, BUTTON_WEIGHT);
-        }
+        localeText = "ui.roller.ambush";
+        combatType = COMBAT_TYPE.AMBUSH;
+        this._stepsAmbush = this._createButton(localeText, combatType);
+        this._stepsPanel.addChild(this._stepsAmbush, BUTTON_WEIGHT);
 
         localeText = "ui.roller.anti_fighter_barrage";
         combatType = COMBAT_TYPE.ANTI_FIGHTER_BARRAGE;
-        widget = this._createButton(localeText, combatType);
-        this._stepsPanel.addChild(widget, BUTTON_WEIGHT);
+        this._stepsAntiFighterBarrage = this._createButton(
+            localeText,
+            combatType
+        );
+        this._stepsPanel.addChild(this._stepsAntiFighterBarrage, BUTTON_WEIGHT);
 
         localeText = "ui.roller.announce_retreat";
         combatType = COMBAT_TYPE.ANNOUNCE_RETREAT;
-        widget = this._createButton(localeText, combatType);
-        this._stepsPanel.addChild(widget, BUTTON_WEIGHT);
+        this._stepsAnnounceRetreat = this._createButton(localeText, combatType);
+        this._stepsPanel.addChild(this._stepsAnnounceRetreat, BUTTON_WEIGHT);
 
         localeText = "ui.roller.space_combat";
         combatType = COMBAT_TYPE.SPACE_COMBAT;
-        widget = this._createButton(localeText, combatType);
-        this._stepsPanel.addChild(widget, BUTTON_WEIGHT);
+        this._stepsSpaceCombat = this._createButton(localeText, combatType);
+        this._stepsPanel.addChild(this._stepsSpaceCombat, BUTTON_WEIGHT);
 
         // GAP
         widget = this._createGap();
@@ -286,62 +241,180 @@ class AutoRollerUI extends HorizontalBox {
 
         localeText = "ui.label.production";
         combatType = COMBAT_TYPE.PRODUCTION;
-        widget = this._createButton(localeText, combatType);
-        this._stepsPanel.addChild(widget, BUTTON_WEIGHT);
+        this._stepsProduction = this._createButton(localeText, combatType);
+        this._stepsPanel.addChild(this._stepsProduction, BUTTON_WEIGHT);
     }
 
-    _fillInvasionPanel(system) {
-        assert(system instanceof System);
+    _fillStepsPanel(system) {
+        assert(!system || system instanceof System);
 
-        this._invasionPanel.removeAllChildren();
+        const enabled = system ? true : false;
+        this._stepsFinishMove.setEnabled(enabled);
+        this._stepsSpaceCannonOffense.setEnabled(enabled);
+        this._stepsAmbush.setEnabled(enabled);
+        this._stepsAntiFighterBarrage.setEnabled(enabled);
+        this._stepsAnnounceRetreat.setEnabled(enabled);
+        this._stepsSpaceCombat.setEnabled(enabled);
+        this._stepsProduction.setEnabled(enabled);
+
+        const ambushAvailable = system && AutoRollerUI._ambush(system);
+        this._stepsAmbush.setEnabled(ambushAvailable);
+    }
+
+    _createAndLayoutInvasionPanel() {
+        assert(!this._invasionPanel);
+
+        this._invasionPanel = new VerticalBox()
+            .setChildDistance(VERTICAL_DISTANCE)
+            .setVerticalAlignment(VerticalAlignment.Fill);
 
         let localeText;
         let combatType;
-        let widget;
 
-        // Always create 3 (but allow more!) for consistent sizing.
-        const numPlanetPanels = Math.max(3, system.planets.length);
-        const planetPanels = new Array(numPlanetPanels).fill(0).map(() => {
-            return new VerticalBox().setChildDistance(VERTICAL_DISTANCE);
-        });
-
-        planetPanels.forEach((panel, index) => {
-            if (index > 0) {
-                widget = this._createGap();
-                this._invasionPanel.addChild(widget, GAP_WEIGHT);
-            }
-            this._invasionPanel.addChild(panel, 4);
-        });
-
-        system.planets.forEach((planet, index) => {
-            const panelIndex = 3 - system.planets.length + index;
-            const panel = planetPanels[panelIndex];
-
-            const planetName = planet.getNameStr();
-            widget = this._createPlanetLabel(planetName);
-            panel.addChild(widget, LABEL_WEIGHT);
+        const createInvasionPlanet = () => {
+            const label = this._createPlanetLabel("?");
+            this._invasionPanel.addChild(label, LABEL_WEIGHT);
 
             localeText = "ui.roller.bombardment";
             combatType = COMBAT_TYPE.BOMBARDMENT;
-            widget = this._createButton(localeText, combatType, planet);
-            panel.addChild(widget, BUTTON_WEIGHT);
+            const bombardment = this._createButton(localeText, combatType);
+            this._invasionPanel.addChild(bombardment, BUTTON_WEIGHT);
 
             localeText = "ui.roller.space_cannon_defense";
             combatType = COMBAT_TYPE.SPACE_CANNON;
-            widget = this._createButton(localeText, combatType, planet);
-            panel.addChild(widget, BUTTON_WEIGHT);
+            const spaceCannonDefense = this._createButton(
+                localeText,
+                combatType
+            );
+            this._invasionPanel.addChild(spaceCannonDefense, BUTTON_WEIGHT);
 
             localeText = "ui.roller.ground_combat";
             combatType = COMBAT_TYPE.GROUND_COMBAT;
-            widget = this._createButton(localeText, combatType, planet);
-            panel.addChild(widget, BUTTON_WEIGHT);
+            const groundCombat = this._createButton(localeText, combatType);
+            this._invasionPanel.addChild(groundCombat, BUTTON_WEIGHT);
+
+            return {
+                label,
+                bombardment,
+                spaceCannonDefense,
+                groundCombat,
+            };
+        };
+
+        this._invasionPlanets = [];
+
+        for (let i = 0; i < 3; i++) {
+            const subPanel = createInvasionPlanet();
+            this._invasionPlanets.push(subPanel);
+            if (i < 2) {
+                const widget = this._createGap();
+                this._invasionPanel.addChild(widget, GAP_WEIGHT);
+            }
+        }
+    }
+
+    _fillInvasionPanel(system) {
+        assert(!system || system instanceof System);
+
+        for (const invasionPlanet of this._invasionPlanets) {
+            invasionPlanet.label.setText("-");
+            invasionPlanet.bombardment.setEnabled(false);
+            invasionPlanet.spaceCannonDefense.setEnabled(false);
+            invasionPlanet.groundCombat.setEnabled(false);
+        }
+
+        const planets = system ? system.planets : [];
+        let combatType;
+        planets.forEach((planet, index) => {
+            if (index >= this._invasionPlanets.length) {
+                return; // only support 3 planets
+            }
+            const invasionPlanet = this._invasionPlanets[index];
+            assert(invasionPlanet);
+
+            const planetName = planet.getNameStr();
+            invasionPlanet.label.setText(planetName);
+
+            combatType = COMBAT_TYPE.BOMBARDMENT;
+            invasionPlanet.bombardment.onClicked.clear();
+            invasionPlanet.bombardment.onClicked.add(
+                ThrottleClickHandler.wrap((button, player) => {
+                    this._onButton(combatType, planet, player);
+                })
+            );
+            invasionPlanet.bombardment.setEnabled(true);
+
+            combatType = COMBAT_TYPE.SPACE_CANNON;
+            invasionPlanet.spaceCannonDefense.onClicked.clear();
+            invasionPlanet.spaceCannonDefense.onClicked.add(
+                ThrottleClickHandler.wrap((button, player) => {
+                    this._onButton(combatType, planet, player);
+                })
+            );
+            invasionPlanet.spaceCannonDefense.setEnabled(true);
+
+            combatType = COMBAT_TYPE.GROUND_COMBAT;
+            invasionPlanet.groundCombat.onClicked.clear();
+            invasionPlanet.groundCombat.onClicked.add(
+                ThrottleClickHandler.wrap((button, player) => {
+                    this._onButton(combatType, planet, player);
+                })
+            );
+            invasionPlanet.groundCombat.setEnabled(true);
         });
     }
 
-    _fillArenaPanel(system) {
-        assert(system instanceof System);
+    _createAndLayoutArenaPanel() {
+        assert(!this._arenaPanel);
+        this._arenaPanel = new VerticalBox()
+            .setChildDistance(VERTICAL_DISTANCE)
+            .setVerticalAlignment(VerticalAlignment.Fill);
 
-        this._arenaLowerLeft.removeAllChildren();
+        // Divide arena panel up.
+        this._arenaBox = new LayoutBox()
+            .setHorizontalAlignment(HorizontalAlignment.Center)
+            .setVerticalAlignment(VerticalAlignment.Center);
+        const arenaBoxBorder = new Border()
+            .setColor(ColorUtil.colorFromHex("#101010"))
+            .setChild(this._arenaBox);
+        this._arenaLowerLeft = new VerticalBox().setChildDistance(
+            VERTICAL_DISTANCE
+        );
+        const arenaLowerRight = new VerticalBox();
+        const arenaLower = new HorizontalBox()
+            .setChildDistance(CONFIG.spacing)
+            .addChild(this._arenaLowerLeft, 1)
+            .addChild(new Border().setColor(CONFIG.spacerColor))
+            .addChild(arenaLowerRight, 1);
+        this._arenaPanel.addChild(arenaBoxBorder, 9);
+        this._arenaPanel.addChild(arenaLower, 3);
+
+        arenaLowerRight
+            .addChild(
+                new Text()
+                    .setFontSize(HELP_FONT_SIZE)
+                    .setText(locale("ui.help.numpad"))
+            )
+            .addChild(
+                new Text()
+                    .setFontSize(HELP_FONT_SIZE)
+                    .setText(locale("ui.help.numpad.4"))
+            )
+            .addChild(
+                new Text()
+                    .setFontSize(HELP_FONT_SIZE)
+                    .setText(locale("ui.help.numpad.5"))
+            )
+            .addChild(
+                new Text()
+                    .setFontSize(HELP_FONT_SIZE)
+                    .setText(locale("ui.help.numpad.9"))
+            )
+            .addChild(
+                new Text()
+                    .setFontSize(HELP_FONT_SIZE)
+                    .setText(locale("ui.help.numpad.0"))
+            );
 
         let localeText;
         let combatType;
@@ -349,60 +422,52 @@ class AutoRollerUI extends HorizontalBox {
 
         localeText = "ui.roller.warp_in";
         combatType = "n/a";
-        widget = this._createButton(localeText, combatType);
-        widget.onClicked.clear();
-        widget.onClicked.add((button, player) => {
+        this._arenaWarpIn = this._createButton(localeText, combatType);
+        this._arenaWarpIn.onClicked.clear();
+        this._arenaWarpIn.onClicked.add((button, player) => {
             AutoRollerArena.warpIn();
         });
-        this._arenaLowerLeft.addChild(widget, BUTTON_WEIGHT);
+        this._arenaLowerLeft.addChild(this._arenaWarpIn, BUTTON_WEIGHT);
 
         localeText = "ui.roller.warp_out";
         combatType = "n/a";
-        widget = this._createButton(localeText, combatType);
-        widget.onClicked.clear();
-        widget.onClicked.add((button, player) => {
+        this._arenaWarpOut = this._createButton(localeText, combatType);
+        this._arenaWarpOut.onClicked.clear();
+        this._arenaWarpOut.onClicked.add((button, player) => {
             AutoRollerArena.warpOut();
         });
-        this._arenaLowerLeft.addChild(widget, BUTTON_WEIGHT);
+        this._arenaLowerLeft.addChild(this._arenaWarpOut, BUTTON_WEIGHT);
 
         localeText = "ui.roller.report_modifiers";
         combatType = COMBAT_TYPE.REPORT_MODIFIERS;
         widget = this._createButton(localeText, combatType);
         this._arenaLowerLeft.addChild(widget, BUTTON_WEIGHT);
 
-        const imgPath = system.raw.img;
         const size = CONFIG.scale * 340;
-        const img = new ImageWidget()
-            .setImage(imgPath, refPackageId)
-            .setImageSize(size, size);
-        this._arenaBox.setChild(img);
+        this._systemImage = new ImageWidget().setImageSize(size, size);
+        this._arenaBox.setChild(this._systemImage);
+    }
 
-        System;
+    _fillArenaPanel(system) {
+        assert(!system || system instanceof System);
+
+        const enabled = system ? true : false;
+        this._arenaWarpIn.setEnabled(enabled);
+        this._arenaWarpOut.setEnabled(enabled);
+
+        const imgPath = system ? system.raw.img : "global/ui/tiles/blank.png";
+        const tint = system ? [1, 1, 1, 1] : [0.1, 0.1, 0.1, 1];
+        this._systemImage.setImage(imgPath, refPackageId);
+        this._systemImage.setTintColor(tint);
     }
 
     /**
      * Reset for "no system activated".
      */
     resetAwaitingSystemActivation() {
-        this._stepsPanel.removeAllChildren();
-        this._invasionPanel.removeAllChildren();
-        this._arenaLowerLeft.removeAllChildren();
-
-        const message = new Text()
-            .setAutoWrap(true)
-            .setFontSize(CONFIG.fontSize)
-            .setText(locale("ui.message.no_system_activated"));
-
-        let localeText;
-        let combatType;
-        let widget;
-
-        localeText = "ui.roller.report_modifiers";
-        combatType = COMBAT_TYPE.REPORT_MODIFIERS;
-        widget = this._createButton(localeText, combatType);
-        this._arenaLowerLeft.addChild(widget, BUTTON_WEIGHT);
-
-        this._arenaBox.setChild(message);
+        this._fillStepsPanel(undefined);
+        this._fillInvasionPanel(undefined);
+        this._fillArenaPanel(undefined);
     }
 
     /**
