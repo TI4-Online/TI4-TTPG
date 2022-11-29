c7758abbb27ce1a1d90d848f43598a1d5356c240
Darrell <darrell.anderson@gmail.com>
2022-11-28 20:41:42 -0800

    do not use a non-constant variable inside a closure, it can change later

--- src/objects/roller/auto-roller-ui.js
+++ src/objects/roller/auto-roller-ui.js
@@ -271,29 +271,26 @@ class AutoRollerUI extends HorizontalBox {
             .setChildDistance(VERTICAL_DISTANCE)
             .setVerticalAlignment(VerticalAlignment.Fill);
 
-        let localeText;
-        let combatType;
-
         const createInvasionPlanet = () => {
             const label = this._createPlanetLabel("?");
             this._invasionPanel.addChild(label, LABEL_WEIGHT);
 
-            localeText = "ui.roller.bombardment";
-            combatType = COMBAT_TYPE.BOMBARDMENT;
-            const bombardment = this._createButton(localeText, combatType);
+            const bombardment = this._createButton(
+                "ui.roller.bombardment",
+                COMBAT_TYPE.BOMBARDMENT
+            );
             this._invasionPanel.addChild(bombardment, BUTTON_WEIGHT);
 
-            localeText = "ui.roller.space_cannon_defense";
-            combatType = COMBAT_TYPE.SPACE_CANNON;
             const spaceCannonDefense = this._createButton(
-                localeText,
-                combatType
+                "ui.roller.space_cannon_defense",
+                COMBAT_TYPE.SPACE_CANNON
             );
             this._invasionPanel.addChild(spaceCannonDefense, BUTTON_WEIGHT);
 
-            localeText = "ui.roller.ground_combat";
-            combatType = COMBAT_TYPE.GROUND_COMBAT;
-            const groundCombat = this._createButton(localeText, combatType);
+            const groundCombat = this._createButton(
+                "ui.roller.ground_combat",
+                COMBAT_TYPE.GROUND_COMBAT
+            );
             this._invasionPanel.addChild(groundCombat, BUTTON_WEIGHT);
 
             return {
@@ -327,7 +324,6 @@ class AutoRollerUI extends HorizontalBox {
         }
 
         const planets = system ? system.planets : [];
-        let combatType;
         planets.forEach((planet, index) => {
             if (index >= this._invasionPlanets.length) {
                 return; // only support 3 planets
@@ -338,29 +334,26 @@ class AutoRollerUI extends HorizontalBox {
             const planetName = planet.getNameStr();
             invasionPlanet.label.setText(planetName);
 
-            combatType = COMBAT_TYPE.BOMBARDMENT;
             invasionPlanet.bombardment.onClicked.clear();
             invasionPlanet.bombardment.onClicked.add(
                 ThrottleClickHandler.wrap((button, player) => {
-                    this._onButton(combatType, planet, player);
+                    this._onButton(COMBAT_TYPE.BOMBARDMENT, planet, player);
                 })
             );
             invasionPlanet.bombardment.setEnabled(true);
 
-            combatType = COMBAT_TYPE.SPACE_CANNON;
             invasionPlanet.spaceCannonDefense.onClicked.clear();
             invasionPlanet.spaceCannonDefense.onClicked.add(
                 ThrottleClickHandler.wrap((button, player) => {
-                    this._onButton(combatType, planet, player);
+                    this._onButton(COMBAT_TYPE.SPACE_CANNON, planet, player);
                 })
             );
             invasionPlanet.spaceCannonDefense.setEnabled(true);
 
-            combatType = COMBAT_TYPE.GROUND_COMBAT;
             invasionPlanet.groundCombat.onClicked.clear();
             invasionPlanet.groundCombat.onClicked.add(
                 ThrottleClickHandler.wrap((button, player) => {
-                    this._onButton(combatType, planet, player);
+                    this._onButton(COMBAT_TYPE.GROUND_COMBAT, planet, player);
                 })
             );
             invasionPlanet.groundCombat.setEnabled(true);
