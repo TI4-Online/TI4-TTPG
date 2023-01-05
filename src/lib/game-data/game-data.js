7b07aa7ba35f2493c9bc8bf572242eb1e2cf94c1
Darrell <darrell.anderson@gmail.com>
2023-01-04 17:00:49 -0800

    remove turn order from history

--- src/lib/game-data/game-data.js
+++ src/lib/game-data/game-data.js
@@ -308,6 +308,7 @@ class GameData {
                 delete playerData.handSummary;
                 delete playerData.maxCommodities;
                 delete playerData.strategyCardsFaceDown;
+                delete playerData.turnOrder;
                 delete playerData.unitModifiers;
                 delete playerData.unitUpgrades;
                 playerData.technologies = playerData.technologies.length;
