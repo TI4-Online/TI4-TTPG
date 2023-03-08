935b2c0fcc504d038c9b46698a6b25d078d288fa
Darrell <darrell.anderson@gmail.com>
2023-03-08 12:32:24 -0800

    add more config values

--- src/lib/game-data/updator-config.js
+++ src/lib/game-data/updator-config.js
@@ -7,11 +7,19 @@ module.exports = (data) => {
     data.isPoK = world.TI4.config.pok;
     data.scoreboard = world.TI4.config.gamePoints;
 
+    if (world.TI4.fogOfWar.isEnabled()) {
+        data.isFogOfWar = true;
+    }
+    if (world.TI4.config.franken) {
+        data.isFranken = true;
+    }
+
     // Gather new config values in a table.
     data.config = {
         codex1: world.TI4.config.codex1,
         codex2: world.TI4.config.codex2,
         codex3: world.TI4.config.codex3,
+        codex4: world.TI4.config.codex4,
         baseMagen: world.TI4.config.baseMagen,
     };
 };
