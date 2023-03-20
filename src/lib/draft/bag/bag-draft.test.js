be3f13f18aad247d21cddbeab3d07dc7601066ff
Darrell <darrell.anderson@gmail.com>
2023-03-20 16:44:53 -0700

    add option to use factions on table for bag draft

--- src/lib/draft/bag/bag-draft.test.js
+++ src/lib/draft/bag/bag-draft.test.js
@@ -4,6 +4,6 @@ const { BagDraft } = require("./bag-draft");
 
 it("draftFactions", () => {
     const count = 13;
-    const factions = BagDraft.draftFactions(count);
+    const factions = BagDraft.draftFactions(count, false);
     assert.equal(factions.length, count);
 });
