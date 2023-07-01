55c781424a231641bac8e1faf7abe9562a27d3c1
Darrell <darrell.anderson@gmail.com>
2023-07-01 10:15:27 -0700

    remove agenda card images, embrace card image widget support

--- src/lib/agenda/agenda-card-widget.js
+++ src/lib/agenda/agenda-card-widget.js
@@ -1,47 +1,13 @@
 const assert = require("../../wrapper/assert-wrapper");
 const { ObjectNamespace } = require("../object-namespace");
-const {
-    Card,
-    ContentButton,
-    ImageButton,
-    ImageWidget,
-    refPackageId,
-} = require("../../wrapper/api");
-
-const TEXTURE_PATH_WITHOUT_SOURCE = "/locale/ui/agenda";
+const { Card, ImageButton, ImageWidget } = require("../../wrapper/api");
 
 /**
  * ImageWidget showing an agenda card.
  *
- * At the moment, keep individual images under "ui".  If and when TTPG can
- * select a card in a cardsheet this could share images with the deck(s).
+ * This used to pull in special images, but TTPG now supports card images.
  */
 class AgendaCardWidget {
-    static __deprecated__setImagePath(widget, card) {
-        assert(widget instanceof ImageWidget || widget instanceof ImageButton);
-        assert(card instanceof Card);
-
-        const agendaNsid = ObjectNamespace.getNsid(card);
-        assert(agendaNsid.startsWith("card.agenda"));
-        const parsed = ObjectNamespace.parseNsid(agendaNsid);
-        assert(parsed);
-
-        if (parsed.source.includes("homebrew")) {
-            // There is no method to read the custom image, pull it from the save JSON.
-            // If no URL do not set the image.
-            const json = JSON.parse(card.toJSONString());
-            const url = json.frontTextureOverride;
-            if (url && url.startsWith("http")) {
-                widget.setImageURL(url);
-            }
-        } else {
-            const path = `${TEXTURE_PATH_WITHOUT_SOURCE}/${parsed.source}/${parsed.name}.jpg`;
-            widget.setImage(path, refPackageId);
-        }
-
-        widget.setImageSize(500, 750);
-    }
-
     static getImageWidget(card) {
         assert(card instanceof Card);
         const widget = new ImageWidget()
@@ -52,12 +18,10 @@ class AgendaCardWidget {
 
     static getImageButton(card) {
         assert(card instanceof Card);
-        const widget = new ImageWidget()
+        const widget = new ImageButton()
             .setImageSize(500, 750)
             .setSourceCard(card);
-
-        // SetSourceCard only available for ImageWidget right now, make image into a button.
-        return new ContentButton().setChild(widget);
+        return widget;
     }
 
     constructor(card) {
