0bd29fbc495d05e113c4873eb8f75fff915b82ac
Darrell <darrell.anderson@gmail.com>
2023-07-18 17:46:27 -0700

    update map when draft settings change

--- src/lib/draft/abstract/ui-draft-settings.js
+++ src/lib/draft/abstract/ui-draft-settings.js
@@ -1,5 +1,6 @@
 const assert = require("../../../wrapper/assert-wrapper");
 const locale = require("../../locale");
+const TriggerableMulticastDelegate = require("../../triggerable-multicast-delegate");
 const { AbstractSliceDraft } = require("./abstract-slice-draft");
 const { ThrottleClickHandler } = require("../../ui/throttle-click-handler");
 const { UiMap } = require("./ui-map");
@@ -21,50 +22,19 @@ const {
 } = require("../../../wrapper/api");
 
 class UiDraftSettings {
-    static _createCheckbox(params) {
-        assert(typeof params.name === "string");
-        assert(typeof params.default === "boolean");
-        assert(typeof params.onCheckStateChanged === "function");
-
-        const checkBox = new CheckBox()
-            .setFontSize(CONFIG.fontSize)
-            .setText(params.name)
-            .setIsChecked(params.default);
-        checkBox.onCheckStateChanged.add(params.onCheckStateChanged);
-        return checkBox;
-    }
-
-    static _createSlider(params) {
-        assert(typeof params.name === "string");
-        assert(typeof params.min === "number");
-        assert(typeof params.max === "number");
-        assert(typeof params.default === "number");
-        assert(typeof params.onValueChanged === "function");
-
-        assert(params.min <= params.max);
-        assert(params.min <= params.default && params.default <= params.max);
-
-        const label = new Text()
-            .setFontSize(CONFIG.fontSize)
-            .setText(params.name);
-        const slider = new Slider()
-            .setFontSize(CONFIG.fontSize)
-            .setTextBoxWidth(CONFIG.fontSize * 4)
-            .setMinValue(params.min)
-            .setMaxValue(params.max)
-            .setStepSize(params.stepSize || 1)
-            .setValue(params.default);
-        slider.onValueChanged.add(params.onValueChanged);
-        const panel = new HorizontalBox()
-            .setChildDistance(CONFIG.spacing)
-            .addChild(label, 1)
-            .addChild(slider, 1);
-        return panel;
-    }
-
     constructor(sliceDraft) {
         assert(sliceDraft instanceof AbstractSliceDraft);
+
         this._sliceDraft = sliceDraft;
+
+        this._onDraftSettingsChanged = new TriggerableMulticastDelegate();
+        this._triggerOnDraftSettingsChanged = () => {
+            this._onDraftSettingsChanged.trigger();
+        };
+    }
+
+    get onDraftSettingsChanged() {
+        return this._onDraftSettingsChanged;
     }
 
     getWidget() {
@@ -156,7 +126,7 @@ class UiDraftSettings {
     _addSliders(panel) {
         // Slice count.
         const sliceGenerator = this._sliceDraft.getSliceGenerator();
-        const sliceCount = UiDraftSettings._createSlider({
+        const sliceCount = this._createSlider({
             name: locale("ui.draft.slice_count"),
             min: sliceGenerator.getMinCount(),
             max: sliceGenerator.getMaxCount(),
@@ -167,7 +137,7 @@ class UiDraftSettings {
 
         // Faction count.
         const factionGenerator = this._sliceDraft.getFactionGenerator();
-        const factionCount = UiDraftSettings._createSlider({
+        const factionCount = this._createSlider({
             name: locale("ui.draft.faction_count"),
             min: factionGenerator.getMinCount(),
             max: factionGenerator.getMaxCount(),
@@ -178,7 +148,7 @@ class UiDraftSettings {
 
         // Add custom sliders AFTER default ones.
         for (const params of this._sliceDraft.getCustomSliders()) {
-            const slider = UiDraftSettings._createSlider(params);
+            const slider = this._createSlider(params);
             panel.addChild(slider);
         }
     }
@@ -186,12 +156,12 @@ class UiDraftSettings {
     _addCheckboxes(panel) {
         // Add custom checkboxes BEFORE default ones.
         for (const params of this._sliceDraft.getCustomCheckBoxes()) {
-            const checkBox = UiDraftSettings._createCheckbox(params);
+            const checkBox = this._createCheckbox(params);
             panel.addChild(checkBox);
         }
 
         // Use faction cards on table?
-        const useFactionsOnTable = UiDraftSettings._createCheckbox({
+        const useFactionsOnTable = this._createCheckbox({
             name: locale("ui.draft.factions_from_cards_short"),
             default: false,
             onCheckStateChanged: () => {},
@@ -230,22 +200,70 @@ class UiDraftSettings {
     }
 
     _addMap(panel) {
-        const options = { includeHomeSystems: false };
-        const { mapString } = UiMap.generateMapString(
-            this._sliceDraft,
-            options
-        );
-        const map = new UiMap()
-            .setMapString(mapString)
-            .setScale(1.5)
-            .createWidget();
-
         const mapBox = new LayoutBox()
             .setHorizontalAlignment(HorizontalAlignment.Center)
-            .setVerticalAlignment(VerticalAlignment.Top)
-            .setChild(map);
-
+            .setVerticalAlignment(VerticalAlignment.Top);
         panel.addChild(mapBox);
+
+        const resetMap = () => {
+            console.log("UiDraftSettings resetMap");
+            const options = { includeHomeSystems: false };
+            const { mapString } = UiMap.generateMapString(
+                this._sliceDraft,
+                options
+            );
+            const map = new UiMap()
+                .setMapString(mapString)
+                .setScale(1.5)
+                .createWidget();
+            mapBox.setChild(map);
+        };
+
+        resetMap();
+        this._onDraftSettingsChanged.add(resetMap);
+    }
+
+    _createCheckbox(params) {
+        assert(typeof params.name === "string");
+        assert(typeof params.default === "boolean");
+        assert(typeof params.onCheckStateChanged === "function");
+
+        const checkBox = new CheckBox()
+            .setFontSize(CONFIG.fontSize)
+            .setText(params.name)
+            .setIsChecked(params.default);
+        checkBox.onCheckStateChanged.add(params.onCheckStateChanged);
+        checkBox.onCheckStateChanged.add(this._triggerOnDraftSettingsChanged);
+        return checkBox;
+    }
+
+    _createSlider(params) {
+        assert(typeof params.name === "string");
+        assert(typeof params.min === "number");
+        assert(typeof params.max === "number");
+        assert(typeof params.default === "number");
+        assert(typeof params.onValueChanged === "function");
+
+        assert(params.min <= params.max);
+        assert(params.min <= params.default && params.default <= params.max);
+
+        const label = new Text()
+            .setFontSize(CONFIG.fontSize)
+            .setText(params.name);
+        const slider = new Slider()
+            .setFontSize(CONFIG.fontSize)
+            .setTextBoxWidth(CONFIG.fontSize * 4)
+            .setMinValue(params.min)
+            .setMaxValue(params.max)
+            .setStepSize(params.stepSize || 1)
+            .setValue(params.default);
+        slider.onValueChanged.add(params.onValueChanged);
+        slider.onValueChanged.add(this._triggerOnDraftSettingsChanged);
+        const panel = new HorizontalBox()
+            .setChildDistance(CONFIG.spacing)
+            .addChild(label, 1)
+            .addChild(slider, 1);
+        return panel;
     }
 }
 
