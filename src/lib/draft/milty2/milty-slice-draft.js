a8495cc830707579a433005e27ccb2b803c3a3ed
Darrell <darrell.anderson@gmail.com>
2023-07-31 16:57:34 -0700

    move milty-slice-layout to separate file

--- src/lib/draft/milty2/milty-slice-draft.js
+++ src/lib/draft/milty2/milty-slice-draft.js
@@ -3,40 +3,14 @@ const {
     AbstractFactionGenerator,
 } = require("../abstract/abstract-faction-generator");
 const { AbstractSliceDraft } = require("../abstract/abstract-slice-draft");
-const { SLICE_SHAPES } = require("../abstract/abstract-slice-generator");
-const { AbstractSliceLayout } = require("../abstract/abstract-slice-layout");
 const { UiDraftSettings } = require("../abstract/ui-draft-settings");
-const { world } = require("../../../wrapper/api");
 const { NavEntry } = require("../../ui/nav/nav-entry");
 const { MiltySliceGenerator } = require("./milty-slice-generator");
+const { MiltySliceLayout } = require("./milty-slice-layout");
 
 /**
  * Milty Draft based on the "abstract slice draft" framework.
  */
-
-/**
- * Set slice shape, with adjustments for 7 or 8 player games.
- */
-class MiltySliceLayout extends AbstractSliceLayout {
-    constructor() {
-        super();
-
-        this.setShape(SLICE_SHAPES.milty);
-
-        if (world.TI4.config.playerCount === 7) {
-            this.setOverrideShape(3, SLICE_SHAPES.milty_7p_seatIndex3);
-        } else if (world.TI4.config.playerCount === 8) {
-            this.setOverrideShape(3, SLICE_SHAPES.milty_7p_seatIndex3);
-            this.setOverrideShape(7, SLICE_SHAPES.milty_7p_seatIndex3);
-        }
-    }
-}
-
-/**
- * Use the default faction generator.
- */
-class MiltyFactionGenerator extends AbstractFactionGenerator {}
-
 class MiltySliceDraft extends AbstractSliceDraft {
     static createDraftSettingsWidget() {
         const sliceDraft = new MiltySliceDraft();
@@ -56,7 +30,7 @@ class MiltySliceDraft extends AbstractSliceDraft {
     constructor() {
         super();
 
-        const miltyFactionGenerator = new MiltyFactionGenerator();
+        const miltyFactionGenerator = new AbstractFactionGenerator(); // no special needs
         const miltySliceLayout = new MiltySliceLayout();
         const miltySliceGenerator = new MiltySliceGenerator();
 
