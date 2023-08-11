72ace6daffd0d6acb24ad1b55914f346757b0bfc
Darrell <darrell.anderson@gmail.com>
2023-08-11 12:47:10 -0700

    temporarily restore scoreboard.js (otherwise existing saves will report an error)

---
+++ src/objects/scoreboard.js
@@ -0,0 +1,3 @@
+// Deprecated.  Leave this file here until existing games wrap.
+// Expect to push the deprecating mod update September 2023.
+console.log("objects/scoreboard.js: DEPRECATED, remove eventually");
