50b77e10b912452eaee9311f668b5f397327c315
Darrell <darrell.anderson@gmail.com>
2023-08-13 12:19:30 -0700

    fix table colors (only applies to exisiting in-progress game saves)

--- src/global/fix-table-colors.js
+++ src/global/fix-table-colors.js
@@ -1,8 +1,12 @@
 const { ColorUtil } = require("../lib/color/color-util");
 const { world } = require("../wrapper/api");
 
+/**
+ * This is necessary for existing save games.  New games start with the expected colors.
+ * Retire this eventually (expected launch SEPT 2023).
+ */
 for (const table of world.getAllTables()) {
-    if (table.getTemplateMetadata() === "table:base/6p_skinny") {
+    if (table.getTemplateMetadata().startsWith("table:base/")) {
         // CURRENT:
         table.setPrimaryColor(ColorUtil.colorFromHex("#494949"));
         table.setSecondaryColor(ColorUtil.colorFromHex("#383838"));
