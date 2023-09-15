55b6bacfdc9bc55819cea6002beab27ff081c2e7
Darrell <darrell.anderson@gmail.com>
2023-09-14 22:58:48 -0700

    cleanup

--- src/lib/draft/scpt/malty-draft.js
+++ src/lib/draft/scpt/malty-draft.js
@@ -1,9 +1,16 @@
 const { NavEntry } = require("../../ui/nav/nav-entry");
 const { MALTY_DRAFTS } = require("./malty-draft.data");
 const CONFIG = require("../../../game-ui/game-ui-config");
-const { Border, Button, Text, VerticalBox } = require("../../../wrapper/api");
+const {
+    Border,
+    Button,
+    LayoutBox,
+    Text,
+    VerticalBox,
+} = require("../../../wrapper/api");
 const { MiltySliceDraft } = require("../milty2/milty-slice-draft");
 const { ThrottleClickHandler } = require("../../ui/throttle-click-handler");
+const locale = require("../../locale");
 
 class MaltyDraft {
     static createDraftNavEntry() {
@@ -21,16 +28,32 @@ class MaltyDraft {
             .setFontSize(CONFIG.fontSize)
             .setText("WTF is this?  Ask Brassbird.");
         const spacer = new Border().setColor(CONFIG.spacerColor);
+        const scrolling = new VerticalBox().setChildDistance(CONFIG.spacing);
+        let sliceDraft = undefined;
+        const cancel = new Button()
+            .setFontSize(CONFIG.fontSize)
+            .setText(locale("ui.button.cancel"));
+        cancel.onClicked.add(
+            ThrottleClickHandler.wrap((button, player) => {
+                if (sliceDraft) {
+                    sliceDraft.cancel();
+                    sliceDraft = undefined;
+                }
+            })
+        );
+
         const panel = new VerticalBox()
             .setChildDistance(CONFIG.spacing)
             .addChild(title)
-            .addChild(spacer);
+            .addChild(spacer)
+            .addChild(scrolling, 1)
+            .addChild(cancel);
 
         for (const maltyDraft of MALTY_DRAFTS) {
             const button = new Button()
                 .setFontSize(CONFIG.fontSize)
                 .setText(maltyDraft.name);
-            panel.addChild(button);
+            scrolling.addChild(button);
             button.onClicked.add(
                 ThrottleClickHandler.wrap((button, player) => {
                     const customInput = [
@@ -38,12 +61,13 @@ class MaltyDraft {
                         `labels=${maltyDraft.labels}`,
                         `factions=${maltyDraft.factions}`,
                     ].join("&");
-                    new MiltySliceDraft()
+                    sliceDraft = new MiltySliceDraft()
                         .setCustomInput(customInput)
                         .start(player);
                 })
             );
         }
+
         return panel;
     }
 }
